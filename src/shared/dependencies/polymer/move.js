9e94d6465c013f9348c8a6cb7276e24c18976acc
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-01-02 15:26:44 +0800

    Update move.js

--- src/shared/dependencies/polymer/move.js
+++ src/shared/dependencies/polymer/move.js
@@ -1,6 +1,8 @@
-const { moveSync, emptyDirSync, readFile } = require('fs-extra');
+const { moveSync, readFileSync, outputFileSync, removeSync } = require('fs-extra');
 const { resolve } = require('path');
 
+
+/* ------------------------------------ */
 
 const fromBuild = resolve( __dirname, '../../../../build' );
 const from = resolve( fromBuild, 'polymer/src/shared/dependencies/polymer/src' );
@@ -12,4 +14,19 @@ moveSync( from, to, {
 });
 
 // 清空原始文件夹
-emptyDirSync( fromBuild );
+removeSync( fromBuild );
+
+/* ------------------------------------ */
+
+const banner = `import babelHelpers from '../polyfill/babelHelpers';\nimport regeneratorRuntime from '../polyfill/regeneratorRuntime';\n\n\n`;
+const polymer = resolve( to, 'index.js' );
+
+// 自动添加一些代码
+outputFileSync(
+  polymer,
+  `${ banner }${
+    readFileSync( polymer ).toString()
+  }`
+);
+
+/* ------------------------------------ */
