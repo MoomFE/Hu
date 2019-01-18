3f34397d67d1270e3a39b4d219eac9bb5f82b632
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-01-18 17:01:27 +0800

    Update babel plugins

--- zen.config.js
+++ zen.config.js
@@ -33,37 +33,12 @@ module.exports = {
     babelrc: {
       plugins: [
         /**
-         * let 和 const 块级作用域
-         */
-        [
-          "@babel/plugin-transform-block-scoping", {
-            "throwIfClosureRequired": true
-          }
-        ],
-        /**
-         * 函数块级作用域
-         */
-        "@babel/plugin-transform-block-scoped-functions",
-        /**
-         * 箭头函数
-         * 
-         * () => {} -> function(){};
-         */
-        "@babel/plugin-transform-arrow-functions",
-        /**
          * 展开运算符
          * 
          * [ 'a', ...array ] -> [ 'a' ].concat( array );
          * fn( ...args )     -> fn.call( void 0, args );
          */
         "@babel/plugin-transform-spread",
-        /**
-         * 对象属性的快捷定义
-         * 
-         * { a, b, c } -> { a: a, b: b, c: c };
-         * { fn(){} }  -> { fn: function(){} };
-         */
-        "@babel/plugin-transform-shorthand-properties",
         /**
          * 赋值解构
          * 
@@ -74,15 +49,6 @@ module.exports = {
           "@babel/plugin-transform-destructuring", {
             "loose": true,
             "useBuiltIns": true
-          }
-        ],
-        /**
-         * 模板字符串
-         * `foo${bar}` -> "foo" + bar
-         */
-        [
-          "@babel/plugin-transform-template-literals", {
-            "loose": true
           }
         ]
       ]
