00ea41661133d543285be1b8c4a2a5067dac65c0
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-02-18 15:17:52 +0800

    优化测试环境代码提示

--- typings/chai.test.d.ts
+++ typings/chai.test.d.ts
@@ -1,2 +1,16 @@
 declare const should: Chai.Should;
 declare const expect: Chai.ExpectStatic;
+
+/**
+ * 返回一个唯一的自定义元素名称
+ */
+declare const customName: string;
+
+interface Window {
+
+  /**
+   * 返回一个唯一的自定义元素名称
+   */
+  customName: string;
+
+}
