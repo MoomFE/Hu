54e99fafae09506c2f4623f84b81a1307245f896
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-02-21 09:40:16 +0800

    完善渲染方法相关单元测试

--- test/Lit/define.render.test.js
+++ test/Lit/define.render.test.js
@@ -11,14 +11,72 @@ describe( 'Lit.define - render', () => {
     });
 
     const div = document.createElement('div').$html(`<${ customName }></${ customName }>`);
-    // const custom = div.firstElementChild;
-    // const lit = custom.$lit;
 
     expect( isRender ).to.be.false;
 
     div.$appendTo( document.body );
 
     expect( isRender ).to.be.true;
+
+    div.$remove();
+  });
+
+  it( 'Lit 实例拥有 $forceUpdate 方法, 迫使 Lit 实例重新渲染', () => {
+    const customName = window.customName;
+    let num = 0;
+
+    Lit.define( customName, {
+      render(){
+        num++;
+      }
+    });
+
+    const div = document.createElement('div').$html(`<${ customName }></${ customName }>`).$appendTo( document.body );
+    const custom = div.firstElementChild;
+    const lit = custom.$lit;
+
+    expect( num ).to.equals( 1 );
+
+    lit.$forceUpdate();
+    expect( num ).to.equals( 2 );
+
+    lit.$forceUpdate();
+    lit.$forceUpdate();
+    expect( num ).to.equals( 4 );
+
+    div.$remove();
+  });
+
+  it( 'render 方法首个参数 html 是一个方法, 用于创建模板字符串', () => {
+    const customName = window.customName;
+
+    Lit.define( customName, {
+      render( html ){
+        expect( html ).to.be.an( 'function' );
+      }
+    });
+
+    document.createElement('div').$html(`<${ customName }></${ customName }>`).$appendTo( document.body ).$remove();;
+  });
+
+  it( 'render 方法返回使用首个参数 html 创建的模板字符串的对象用于渲染', () => {
+    const customName = window.customName;
+
+    Lit.define( customName, {
+      render( html ){
+        return html`<div>123</div>`
+      }
+    });
+
+    const div = document.createElement('div').$html(`<${ customName }></${ customName }>`).$appendTo( document.body );
+    const custom = div.firstElementChild;
+    const $el = custom.$lit.$el;
+
+    expect( $el.children.length ).to.equals( 1 );
+    expect( $el.children[0]._nodeName ).to.equals( 'div' );
+    expect( $el.children[0].innerHTML ).to.equals( '123' );
+
+    div.$remove();
   });
 
 });
