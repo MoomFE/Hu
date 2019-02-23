73963a28e736c1461de407f8f2e2ccb1422b0280
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-02-23 09:02:03 +0800

    完善 computed 相关单元测试

--- test/Lit/define.computed.test.js
+++ test/Lit/define.computed.test.js
@@ -18,4 +18,67 @@ describe( 'Lit.define - computed', () => {
       .that.is.deep.equals({ a: 123 });
   });
 
+  it( '计算属性在依赖于创建了观察者的目标对象时, 目标对象被更改, 计算属性自动更新 ( 一 )', () => {
+    const customName = window.customName;
+
+    Lit.define( customName, {
+      data(){
+        return {
+          a: 1,
+          b: 2
+        };
+      },
+      computed: {
+        c(){
+          return this.a + this.b;
+        }
+      }
+    });
+
+    const div = document.createElement('div').$html(`<${ customName }></${ customName }>`);
+    const custom = div.firstElementChild;
+    const lit = custom.$lit;
+
+    expect( lit.$computed ).has.property( 'c' ).that.is.equals( 3 );
+
+    lit.a = 2;
+
+    expect( lit.$computed ).has.property( 'c' ).that.is.equals( 4 );
+
+    lit.b = 998;
+
+    expect( lit.$computed ).has.property( 'c' ).that.is.equals( 1000 );
+  });
+
+  it( '计算属性在依赖于创建了观察者的目标对象时, 目标对象被更改, 计算属性自动更新 ( 二 )', () => {
+    const customName = window.customName;
+    const data = Lit.observable({
+      a: 1,
+      b: 2
+    });
+
+    Lit.define( customName, {
+      computed: {
+        c(){
+          return data.a + data.b;
+        }
+      }
+    });
+
+    const div = document.createElement('div').$html(`<${ customName }></${ customName }>`);
+    const custom = div.firstElementChild;
+    const lit = custom.$lit;
+    
+
+    expect( lit.$computed ).has.property( 'c' ).that.is.equals( 3 );
+
+    data.a = 2;
+
+    expect( lit.$computed ).has.property( 'c' ).that.is.equals( 4 );
+
+    data.b = 998;
+
+    expect( lit.$computed ).has.property( 'c' ).that.is.equals( 1000 );
+  });
+
 });
