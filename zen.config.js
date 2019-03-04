7482fb1f67821ef022b7d6a3693aa91b0053e4ef
Zhang-Wei-666 <994039348@qq.com>
2019-03-05 00:17:35 +0800

    Update zen.config.js

--- zen.config.js
+++ zen.config.js
@@ -66,30 +66,7 @@ module.exports = {
 
     rollup: true,
 
-    babel: true,
-    babelrc: {
-      plugins: [
-        /**
-         * 展开运算符
-         * 
-         * [ 'a', ...array ] -> [ 'a' ].concat( array );
-         * fn( ...args )     -> fn.call( void 0, args );
-         */
-        "@babel/plugin-transform-spread",
-        /**
-         * 赋值解构
-         * 
-         * [ a, b ] = arr;   -> a = arr[0]; b = arr[1];
-         * { a: 1, ...obj }; -> Object.assign( { a: 1 }, obj );
-         */
-        [
-          "@babel/plugin-transform-destructuring", {
-            "loose": true,
-            "useBuiltIns": true
-          }
-        ]
-      ]
-    },
+    babel: false,
 
     terserOptions: {
       ecma: 6,
