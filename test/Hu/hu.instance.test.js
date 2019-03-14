b4d10aa03050a64e6f659635f4c167afdd15f4d5
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-03-14 16:01:06 +0800

    完善单元测试

--- test/Hu/hu.instance.test.js
+++ test/Hu/hu.instance.test.js
@@ -22,6 +22,35 @@ describe( 'Hu.instance', () => {
     expect( hu.$options ).is.deep.equals({ asd: 123, data });
   });
 
+  it( '实例创建后在实例上会有 $info 选项, 包含了当前实例的各种信息, 且不可更改', () => {
+    const customName = window.customName;
+
+    Hu.define( customName );
+
+    const div = document.createElement('div').$html(`<${ customName }></${ customName }>`);
+    const custom = div.firstElementChild;
+    const hu = custom.$hu;
+
+    expect( hu.$info.name ).is.equals( customName );
+
+    for( const name of Reflect.ownKeys( hu.$info ) ){
+      const value = hu.$info[ name ];
+      const type = Object.prototype.toString.call( value );
+
+      delete hu.$info[ name ];
+      expect( hu.$info[ name ] ).is.equals( value );
+      expect( Object.prototype.toString.call( hu.$info[ name ] ) ).is.equals( type );
+
+      hu.$info[ name ] = 123;
+      expect( hu.$info[ name ] ).is.equals( value );
+      expect( Object.prototype.toString.call( hu.$info[ name ] ) ).is.equals( type );
+
+      hu.$info[ name ] = '123';
+      expect( hu.$info[ name ] ).is.equals( value );
+      expect( Object.prototype.toString.call( hu.$info[ name ] ) ).is.equals( type );
+    }
+  });
+
   it( '实例创建后所有前缀为 $ 的私有选项全部不能进行修改及删除', () => {
     const customName = window.customName;
 
