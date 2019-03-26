87c2daa329839feada78e05c5490db2afff05257
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-03-26 09:55:10 +0800

    完善文档文件

--- typings/chai.test.d.ts
+++ typings/chai.test.d.ts
@@ -6,11 +6,27 @@ declare const expect: Chai.ExpectStatic;
  */
 declare const customName: string;
 
+/**
+ * 自定义事件的触发
+ * @param target 需要触发事件的对象
+ * @param type 需要触发的事件名称
+ * @param process 其它处理
+ */
+declare const triggerEvent: ( target: Element, type: String, process: Function ) => void;
+
 interface Window {
 
   /**
    * 返回一个唯一的自定义元素名称
    */
   customName: string;
+
+  /**
+   * 自定义事件的触发
+   * @param target 需要触发事件的对象
+   * @param type 需要触发的事件名称
+   * @param process 其它处理
+   */
+  triggerEvent( target: Element, type: String, process: Function ): void;
 
 }
