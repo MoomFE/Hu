a2ce4e5519c1844959b384301a7db13cd54c0955
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-03-28 16:45:43 +0800

    移植 lit-html 的 unsafeHTML 指令方法到项目内, 方便后续调整

--- src/html/directive/unsafeHTML.js
+++ src/html/directive/unsafeHTML.js
@@ -1,4 +1,38 @@
-import { unsafeHTML } from 'lit-html/directives/unsafe-html';
+import { directive, NodePart } from 'lit-html';
+import isPrimitive from '../../shared/util/isPrimitive';
 
 
-export default unsafeHTML;
+/**
+ * lit-html
+ * directives/unsafeHTML
+ * Licensed under the MIT License
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * modified by Wei Zhang (@Zhang-Wei-666)
+ */
+
+const oldValueMap = new WeakMap();
+
+export default directive( value => part => {
+  if( !( part instanceof NodePart ) ){
+    throw new Error('unsafe / unsafeHTML 方法只能在文本区域绑定中使用 !');
+  }
+
+  const oldValue = oldValueMap.get( part );
+
+  if( oldValue && isPrimitive( value ) && value === oldValue.value && part.value === oldValue.fragment ){
+    return;
+  }
+
+  const template = document.createElement('template');
+        template.innerHTML = value;
+
+  const fragment = document.importNode( template.content, true );
+
+  part.setValue( fragment );
+
+  oldValueMap.set( part, {
+    value,
+    fragment
+  });
+});
