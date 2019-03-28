0534c075f4a8e040176a138410bcce907efbf32d
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-03-28 18:19:12 +0800

    完善错误处理

--- src/html/directive/bind.js
+++ src/html/directive/bind.js
@@ -1,39 +1,50 @@
-import { directive } from 'lit-html';
+import { directive, AttributePart } from 'lit-html';
 import { observeProxyMap } from '../../static/observable/observe';
 import { bindWatchesMap, renderStack } from '../const';
 import $watch from '../../shared/global/Hu/prototype/$watch';
 
 
-export default directive(( proxy, name ) => part => {
+export default directive(( proxy, name ) => {
+
+  // 是否是观察者对象
   // 绑定的必须是观察者对象
-  if( !observeProxyMap.has( proxy ) ){
-    return;
-  }
+  const isObserve = observeProxyMap.has( proxy );
 
-  const {
-    name: attr,
-    element
-  } = part.committer;
-
-  const unWatch = $watch(
-    () => proxy[ name ],
-    ( value ) => {
-      return element.setAttribute( attr, value );
-    },
-    {
-      immediate: true
+  return part => {
+    if( !( part instanceof AttributePart ) ){
+      throw new Error('Hu.html.bind 指令方法只能在元素属性绑定中使用 !');
     }
-  );
 
-  // 当前渲染元素
-  const rendering = renderStack[ renderStack.length - 1 ];
-  // 当前渲染元素属性监听解绑方法集
-  let bindWatches = bindWatchesMap.get( rendering );
+    const {
+      name: attr,
+      element
+    } = part.committer;
 
-  if( !bindWatches ){
-    bindWatches = [];
-    bindWatchesMap.set( rendering, bindWatches );
-  }
+    if( !isObserve ){
+      element.setAttribute( attr, proxy[ name ] );
+      return;
+    }
 
-  bindWatches.push( unWatch );
+    const unWatch = $watch(
+      () => proxy[ name ],
+      ( value ) => {
+        return element.setAttribute( attr, value );
+      },
+      {
+        immediate: true
+      }
+    );
+  
+    // 当前渲染元素
+    const rendering = renderStack[ renderStack.length - 1 ];
+    // 当前渲染元素属性监听解绑方法集
+    let bindWatches = bindWatchesMap.get( rendering );
+  
+    if( !bindWatches ){
+      bindWatches = [];
+      bindWatchesMap.set( rendering, bindWatches );
+    }
+  
+    bindWatches.push( unWatch );
+  };
 });
