55a1e1592339567e54325ab0a200551e8116c61b
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-03-29 16:38:55 +0800

    完善单元测试

--- test/Hu/hu.html.directive.test.js
+++ test/Hu/hu.html.directive.test.js
@@ -290,4 +290,72 @@ describe( 'Hu.html.directive', () => {
     });
   });
 
+  it( 'html.bind: 该指令方法可使用 :class 指令对元素 className 进行绑定', ( done ) => {
+    const bind = Hu.html.bind;
+    const div = document.createElement('div');
+    const data = Hu.observable({
+      class: {
+        a: true,
+        b: false
+      }
+    });
+
+    Hu.render( div )`
+      <div :class=${ bind( data, 'class' ) }></div>
+    `;
+
+    expect( div.firstElementChild.className ).is.equals( 'a' );
+
+    data.class = {
+      a: false,
+      b: true
+    };
+
+    expect( div.firstElementChild.className ).is.equals( 'a' );
+    Hu.nextTick(() => {
+      expect( div.firstElementChild.className ).is.equals( 'b' );
+
+      done();
+    });
+  });
+
+  it( 'html.bind: 该指令方法可使用 :style 指令对元素 className 进行绑定', ( done ) => {
+    const bind = Hu.html.bind;
+    const div = document.createElement('div');
+    const data = Hu.observable({
+      style: {
+        fontSize: '12px',
+        width: '100px'
+      }
+    });
+
+    Hu.render( div )`
+      <div :style=${ bind( data, 'style' ) }></div>
+    `;
+
+    expect( div.firstElementChild.style ).is.deep.include({
+      fontSize: '12px',
+      width: '100px'
+    });
+
+    data.style = {
+      fontSize: '16px',
+      width: '120px'
+    };
+
+    expect( div.firstElementChild.style ).is.deep.include({
+      fontSize: '12px',
+      width: '100px'
+    });
+    Hu.nextTick(() => {
+      expect( div.firstElementChild.style ).is.deep.include({
+        fontSize: '16px',
+        width: '120px'
+      });
+
+      done();
+    });
+  });
+
+
 });
