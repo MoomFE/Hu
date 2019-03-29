23502bcbb58552ffe7c0aa99e0663dcb3b159921
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-03-29 18:40:33 +0800

    Update model.js

--- src/html/parts/model.js
+++ src/html/parts/model.js
@@ -1,3 +1,4 @@
+import { isArray } from "../../shared/global/Array/index";
 
 
 export default class ModelPart{
@@ -6,12 +7,24 @@ export default class ModelPart{
     this.elem = element;
   }
 
-  setValue(){
+  setValue( options ){
+    if( !( isArray( options ) && options.length > 1 ) ){
+      throw new Error(':model 指令的参数出错, :model 指令不支持此种传参 !');
+    }
 
+    this.oldOptions = this.options;
+    this.options = options;
   }
 
   commit(){
+    const [ oldProxy, oldName ] = this.oldOptions || [];
+    const [ proxy, name ] = this.options;
 
+    if( oldProxy === proxy && oldName === name ){
+      return;
+    }
+
+    
   }
 
 }
