bef1304604d9133413f97c8039f38ea6c1a22e30
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-04-01 11:15:16 +0800

    完善 :model 指令对 select 控件的单项绑定

--- src/html/parts/model.js
+++ src/html/parts/model.js
@@ -1,10 +1,22 @@
 import { isArray } from "../../shared/global/Array/index";
+import { filter } from "../../shared/global/Array/prototype";
+import addEventListener from "../../shared/util/addEventListener";
+import emptyObject from "../../shared/const/emptyObject";
 
 
 export default class ModelPart{
 
   constructor( element ){
+    const tag = element.nodeName.toLowerCase();
+    const type = element.type;
+    let handler;
+
+    if( tag === 'select' ){
+      handler = handlerSelect;
+    }
+
     this.elem = element;
+    this.handler = handler;
   }
 
   setValue( options ){
@@ -12,19 +24,27 @@ export default class ModelPart{
       throw new Error(':model 指令的参数出错, :model 指令不支持此种传参 !');
     }
 
-    this.oldOptions = this.options;
+    this.oldOptions = this.options || emptyObject;
     this.options = options;
   }
 
   commit(){
-    const [ oldProxy, oldName ] = this.oldOptions || [];
-    const [ proxy, name ] = this.options;
-
-    if( oldProxy === proxy && oldName === name ){
+    if( this.init || this.oldOptions[0] === this.options[0] && this.oldOptions[1] === this.options[1] ){
       return;
     }
 
-    
+    this.init = true;
+    this.handler && this.handler( this, this.elem );
   }
 
+}
+
+function handlerSelect( part, elem ){
+  addEventListener( elem, 'change', event => {
+    const [ proxy, name ] = part.options;
+    const value = filter.call( elem.options, option => option.selected )
+                        .map( option => option.value );
+
+    proxy[ name ] = elem.multiple ? value : value[0];
+  });
 }
