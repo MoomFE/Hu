0ebda4c4ed510f70cde344ee4d1df68b23a12b92
Zhang-Wei-666 <994039348@qq.com>
2019-04-04 22:48:49 +0800

    完善单元测试

--- test/Hu/define.mixins.test.js
+++ test/Hu/define.mixins.test.js
@@ -74,7 +74,7 @@ describe( 'Hu.define - mixins', () => {
     });
   });
 
-  it( '使用 mixins 选项对当前实例 props 选项进行混入, 多个 mixin 时, 越后定义的 mixin 优先级越高', () => {
+  it( '使用 mixins 选项对当前实例 props 选项进行混入, 多个 mixin 时, 越后定义的 prop 优先级越高', () => {
     const hu = new Hu({
       mixins: [
         { props: { a: { default: 'a1' } } },
@@ -88,7 +88,7 @@ describe( 'Hu.define - mixins', () => {
     });
   });
 
-  it( '使用 mixins 选项对当前实例 props 选项进行混入, 多个 mixin 时, 越后定义的 mixin 优先级越高 ( Vue )', () => {
+  it( '使用 mixins 选项对当前实例 props 选项进行混入, 多个 mixin 时, 越后定义的 prop 优先级越高 ( Vue )', () => {
     const vm = new Vue({
       mixins: [
         { props: { a: { default: 'a1' } } },
@@ -100,6 +100,88 @@ describe( 'Hu.define - mixins', () => {
     expect( vm.$props ).is.deep.equals({
       a: 'a3'
     });
+  });
+
+  it( '使用 mixins 选项对当前实例生命周期回调进行混入', () => {
+    const result = [];
+    const hu = new Hu({
+      mixins: [
+        {
+          created(){ result.push( 1 ) }
+        }
+      ],
+      created(){ result.push( 2 ) }
+    });
+
+    expect( result ).is.deep.equals([ 1, 2 ])
+  });
+
+  it( '使用 mixins 选项对当前实例生命周期回调进行混入 ( Vue )', () => {
+    const result = [];
+    const vm = new Vue({
+      mixins: [
+        {
+          created(){ result.push( 1 ) }
+        }
+      ],
+      created(){ result.push( 2 ) }
+    });
+
+    expect( result ).is.deep.equals([ 1, 2 ])
+  });
+
+  it( '使用 mixins 选项对当前实例生命周期回调进行混入, 当前实例定义的生命周期回调最晚执行', () => {
+    const result = [];
+    const hu = new Hu({
+      mixins: [
+        { created(){ result.push( 1 ) } },
+        { created(){ result.push( 2 ) } },
+        { created(){ result.push( 3 ) } }
+      ],
+      created(){ result.push( 4 ) }
+    });
+
+    expect( result ).is.deep.equals([ 1, 2, 3, 4 ])
+  });
+
+  it( '使用 mixins 选项对当前实例生命周期回调进行混入, 当前实例定义的生命周期回调最晚执行 ( Vue )', () => {
+    const result = [];
+    const vm = new Vue({
+      mixins: [
+        { created(){ result.push( 1 ) } },
+        { created(){ result.push( 2 ) } },
+        { created(){ result.push( 3 ) } }
+      ],
+      created(){ result.push( 4 ) }
+    });
+
+    expect( result ).is.deep.equals([ 1, 2, 3, 4 ])
+  });
+
+  it( '使用 mixins 选项对当前实例生命周期回调进行混入, 多个 mixin 时, 越后定义的 mixin 内的生命周期回调越晚执行', () => {
+    const result = [];
+    const hu = new Hu({
+      mixins: [
+        { created(){ result.push( 1 ) } },
+        { created(){ result.push( 2 ) } },
+        { created(){ result.push( 3 ) } }
+      ]
+    });
+
+    expect( result ).is.deep.equals([ 1, 2, 3 ])
+  });
+
+  it( '使用 mixins 选项对当前实例生命周期回调进行混入, 多个 mixin 时, 越后定义的 mixin 内的生命周期回调越晚执行 ( Vue )', () => {
+    const result = [];
+    const vm = new Vue({
+      mixins: [
+        { created(){ result.push( 1 ) } },
+        { created(){ result.push( 2 ) } },
+        { created(){ result.push( 3 ) } }
+      ]
+    });
+
+    expect( result ).is.deep.equals([ 1, 2, 3 ])
   });
 
 });
