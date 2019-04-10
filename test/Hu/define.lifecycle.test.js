df1eb1a50c6e251805a53e85ab2ffd73a7f87c32
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-04-10 16:20:46 +0800

    完善单元测试

--- test/Hu/define.lifecycle.test.js
+++ test/Hu/define.lifecycle.test.js
@@ -396,6 +396,228 @@ describe( 'Hu.define - lifecycle', () => {
     custom.$remove();
   });
 
+  it( 'beforeDestroy 声明周期回调: 实例销毁之前调用, 在这一步, 实例仍然完全可用', () => {
+    let index = 0;
+    const hu = new Hu({
+      data: {
+        a: 1
+      },
+      computed: {
+        b(){
+          return this.a * 2;
+        }
+      },
+      beforeDestroy(){
+        index++;
+        expect( this.b ).is.equals( 2 );
+      }
+    });
+
+    expect( index ).is.equals( 0 );
+
+    hu.$destroy();
+
+    expect( index ).is.equals( 1 );
+  });
+
+  it( 'beforeDestroy 声明周期回调: 实例销毁之前调用, 在这一步, 实例仍然完全可用 ( 自定义元素 )', () => {
+    const customName = window.customName;
+    let index = 0;
+
+    Hu.define( customName, {
+      data: () => ({
+        a: 1
+      }),
+      computed: {
+        b(){
+          return this.a * 2;
+        }
+      },
+      beforeDestroy(){
+        index++;
+        expect( this.b ).is.equals( 2 );
+      }
+    });
+
+    const custom = document.createElement( customName );
+    const hu = custom.$hu;
+
+    expect( index ).is.equals( 0 );
+
+    hu.$destroy();
+
+    expect( index ).is.equals( 1 );
+  });
+
+  it( 'beforeDestroy 声明周期回调: 实例销毁后调用', () => {
+    let index = 0;
+    const hu = new Hu({
+      data: {
+        a: 1
+      },
+      computed: {
+        b(){
+          return this.a * 2;
+        }
+      },
+      destroyed(){
+        index++;
+        expect( this.b ).is.equals( undefined );
+      }
+    });
+
+    expect( index ).is.equals( 0 );
+
+    hu.$destroy();
+
+    expect( index ).is.equals( 1 );
+  });
+
+  it( 'beforeDestroy 声明周期回调: 实例销毁后调用 ( 自定义元素 )', () => {
+    const customName = window.customName;
+    let index = 0;
+
+    Hu.define( customName, {
+      data: () => ({
+        a: 1
+      }),
+      computed: {
+        b(){
+          return this.a * 2;
+        }
+      },
+      destroyed(){
+        index++;
+        expect( this.b ).is.equals( undefined );
+      }
+    });
+
+    const custom = document.createElement( customName );
+    const hu = custom.$hu;
+
+    expect( index ).is.equals( 0 );
+
+    hu.$destroy();
+
+    expect( index ).is.equals( 1 );
+  });
+
+  it( 'connected 声明周期回调: 自定义元素被添加到文档流', () => {
+    const customName = window.customName;
+    let index = 0;
+
+    Hu.define( customName, {
+      connected(){
+        index++;
+      }
+    });
+
+    expect( index ).is.equals( 0 );
+
+    const custom = document.createElement( customName );
+
+    expect( index ).is.equals( 0 );
+
+    custom.$appendTo( document.body );
+    expect( index ).is.equals( 1 );
+
+    custom.$remove();
+    expect( index ).is.equals( 1 );
+
+    custom.$appendTo( document.body );
+    expect( index ).is.equals( 2 );
+
+    custom.$remove();
+    expect( index ).is.equals( 2 );
+
+    custom.$appendTo( document.body );
+    expect( index ).is.equals( 3 );
+
+    custom.$remove();
+    expect( index ).is.equals( 3 );
+
+    custom.$appendTo( document.body );
+    expect( index ).is.equals( 4 );
+
+    custom.$remove();
+  });
+
+  it( 'adopted 声明周期回调: 自定义元素被移动到新文档时调用', () => {
+    const customName = window.customName;
+    let index = 0;
+    let result;
+
+    Hu.define( customName, {
+      adopted(){
+        result = [ ...arguments ];
+        index++;
+      }
+    });
+
+    expect( index ).is.equals( 0 );
+    expect( result ).is.undefined;
+
+    const custom = document.createElement( customName );
+
+    expect( index ).is.equals( 0 );
+    expect( result ).is.undefined;
+
+    custom.$appendTo( document.body );
+
+    expect( index ).is.equals( 0 );
+    expect( result ).is.undefined;
+
+    const iframe = document.createElement('iframe').$appendTo( document.body );
+    const iframeDocument = iframe.contentWindow.document;
+    const iframeBody = iframeDocument.body;
+
+    custom.$appendTo( iframeBody );
+
+    expect( index ).is.equals( 1 );
+    expect( result ).is.deep.equals([ iframeDocument, document ]);
+
+    custom.$appendTo( document.body );
+    expect( result ).is.deep.equals([ document, iframeDocument ]);
+
+    expect( index ).is.equals( 2 );
+
+    custom.$remove();
+    iframe.$remove();
+  });
+
+  it( 'disconnected 声明周期回调: 自定义元素被从文档流移除', () => {
+    const customName = window.customName;
+    let index = 0;
+
+    Hu.define( customName, {
+      disconnected(){
+        index++;
+      }
+    });
+
+    expect( index ).is.equals( 0 );
+
+    const custom = document.createElement( customName );
+
+    expect( index ).is.equals( 0 );
+
+    custom.$appendTo( document.body );
+
+    expect( index ).is.equals( 0 );
+
+    custom.$remove();
+
+    expect( index ).is.equals( 1 );
+
+    custom.$appendTo( document.body );
+
+    expect( index ).is.equals( 1 );
+
+    custom.$remove();
+
+    expect( index ).is.equals( 2 );
+  });
+
   it( '生命周期回调的 this 指向的都是当前实例', () => {
     const result = [];
     const returnSelf = function(){
