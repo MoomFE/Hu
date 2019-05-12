eaa2c46b6e7805e44cb04371a5ed859d7a860ffb
Zhang-Wei-666 <994039348@qq.com>
2019-05-13 00:01:55 +0800

    Update isPrimitive.js

--- src/shared/util/isPrimitive.js
+++ src/shared/util/isPrimitive.js
@@ -1,4 +1,12 @@
-import { isPrimitive } from 'lit-html'
+import isObject from "./isObject";
+import isFunction from "./isFunction";
 
 
-export default isPrimitive;
+/**
+ * 判断传入对象是否是原始对象
+ */
+export default value => {
+  return value === null || !(
+    isObject( value ) || isFunction( value )
+  );
+};
