9b8b0ac71f64032a9e017f023798186c17a5e01c
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-05-13 18:36:01 +0800

    Create moveChildNodes.js

---
+++ src/shared/util/moveChildNodes.js
@@ -0,0 +1,10 @@
+
+
+export default ( container, start, end = null, before = null ) => {
+  while( start !== end ){
+    const node = start.nextSibling;
+
+    container.insertBefore( start, before );
+    start = node;
+  }
+}
