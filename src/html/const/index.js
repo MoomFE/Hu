266cff73fd4282ce0421381a735cafbca010875b
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-05-14 09:56:12 +0800

    细节修改

--- src/html/const/index.js
+++ src/html/const/index.js
@@ -34,7 +34,7 @@ export const directives = new WeakSet();
  */
 export const lastAttributeNameRegex = /([ \x09\x0a\x0c\x0d])([^\0-\x1F\x7F-\x9F "'>=/]+)([ \x09\x0a\x0c\x0d]*=[ \x09\x0a\x0c\x0d]*(?:[^ \x09\x0a\x0c\x0d"'`<>=]*|"[^"]*|'[^']*))$/;
 
-export const boundAttributeSuffix = '$hu';
+export const boundAttributeSuffix = '$hu$';
 
 export const marker = `{{hu-${ String( random() ).slice(2) }}}`;
 
