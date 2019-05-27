dca54f20bb3f57387c554c828ce05af188ad9394
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-05-27 16:29:34 +0800

    重构基础指令 .prop

--- test/Lib/hu.js
+++ test/Lib/hu.js
@@ -2280,14 +2280,28 @@
 
   }
 
-  class BasicPropertyDirective extends BasicBooleanDirective{
+  class BasicPropertyDirective{
 
-    commit(){
-      const { value, oldValue } = this;
+    constructor( element, name, strings, modifiers ){
+      if( !isSingleBind( strings ) ){
+        throw new Error('.prop 指令的传值只允许包含单个表达式 !');
+      }
 
-      isEqual( value, oldValue ) || (
-        this.elem[ this.attr ] = value
-      );
+      this.elem = element;
+      this.name = name;
+    }
+
+    commit( value, isDirectiveFn ){
+      // 用户传递的是指令方法
+      // 交给指令方法处理
+      if( isDirectiveFn ) return value( this );
+      // 两次传入的值不同
+      if( isNotEqual( value, this.value ) ){
+        // 存储当前值
+        [ this.value, this.oldValue ] = [ value, this.value ];
+        // 更新属性值
+        this.elem[ this.name ] = value;
+      }
     }
 
   }
