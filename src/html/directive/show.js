82880daf57ecbddf332574ff9fc1ff16e3d22dba
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-05-29 11:20:39 +0800

    重构 :show 指令

--- src/html/directive/show.js
+++ src/html/directive/show.js
@@ -1,10 +1,27 @@
-import TextDirective from './text';
+import isSingleBind from "../util/isSingleBind";
+import { has } from "../../shared/global/Reflect/index";
+import isNotEqual from "../../shared/util/isNotEqual";
 
 
-export default class ShowDirective extends TextDirective{
+export default class ShowDirective{
 
-  commit(){
-    this.elem.style.display = this.value ? '' : 'none'
+  constructor( element, name, strings, modifiers ){
+    if( !isSingleBind( strings ) ){
+      throw new Error(':text 指令的传值只允许包含单个表达式 !');
+    }
+
+    this.elem = element;
+  }
+
+  commit( value, isDirectiveFn ){
+    // 用户传递的是指令方法
+    // 交给指令方法处理
+    if( isDirectiveFn ) return value( this );
+    // 首次设置值或两次传入的值不同
+    if( !has( this, 'value' ) || isNotEqual( value, this.value ) ){
+      this.value = value;
+      this.elem.style.display = value ? '' : 'none'
+    }
   }
 
 }
--- test/Lib/hu.js
+++ test/Lib/hu.js
@@ -1838,10 +1838,25 @@
 
   }
 
-  class ShowDirective extends TextDirective{
+  class ShowDirective{
 
-    commit(){
-      this.elem.style.display = this.value ? '' : 'none';
+    constructor( element, name, strings, modifiers ){
+      if( !isSingleBind( strings ) ){
+        throw new Error(':text 指令的传值只允许包含单个表达式 !');
+      }
+
+      this.elem = element;
+    }
+
+    commit( value, isDirectiveFn ){
+      // 用户传递的是指令方法
+      // 交给指令方法处理
+      if( isDirectiveFn ) return value( this );
+      // 首次设置值或两次传入的值不同
+      if( !has( this, 'value' ) || isNotEqual( value, this.value ) ){
+        this.value = value;
+        this.elem.style.display = value ? '' : 'none';
+      }
     }
 
   }
