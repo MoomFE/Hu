c33212f35bc795cafb9479fef7b51ccfeec2b289
Zhang-Wei-666 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-06-04 16:08:44 +0800

    修复从 repeat 指令方法渲染出的 节点不会被注销的问题

--- src/html/directiveFn/repeat.js
+++ src/html/directiveFn/repeat.js
@@ -5,6 +5,7 @@ import removeNodes from '../../shared/util/removeNodes';
 import moveChildNodes from '../../shared/util/moveChildNodes';
 import createMarker from '../util/createMarker';
 import commitPart from '../util/commitPart';
+import destroyPart from '../util/destroyPart';
 
 
 /**
@@ -22,112 +23,127 @@ const keyListCache = new WeakMap();
 export default directiveFn(( items, key, template ) => {
   const keyFn = isFunction( key ) ? key : item => item[ key ];
 
-  return containerPart => {
-    if( !( containerPart instanceof NodePart ) ){
-      throw new Error('Hu.html.repeat 指令方法只能在文本区域中使用 !');
-    }
-
-    const oldParts = partListCache.get( containerPart ) || [];
-    const oldKeys = keyListCache.get( containerPart ) || [];
-
-    const newKeys = [];
-    const newValues = [];
-    const newParts = [];
-
-    for( let index = 0, item; index < items.length; index++ ){
-      item = items[ index ];
-
-      newKeys[ index ] = keyFn( item, index, items );
-      newValues[ index ] = template( item, index, items );
-    }
-
-    let newKeyToIndexMap;
-    let oldKeyToIndexMap;
-
-    let oldHead = 0;
-    let oldTail = oldParts.length - 1;
-    let newHead = 0;
-    let newTail = newValues.length - 1;
-
-
-    while( oldHead <= oldTail && newHead <= newTail ){
-      if( oldParts[ oldHead ] === null ){
-        oldHead++;
+  return [
+    /**
+     * commit
+     */
+    containerPart => {
+      if( !( containerPart instanceof NodePart ) ){
+        throw new Error('Hu.html.repeat 指令方法只能在文本区域中使用 !');
       }
-      else if( oldParts[ oldTail ] === null ){
-        oldTail--;
+  
+      const oldParts = partListCache.get( containerPart ) || [];
+      const oldKeys = keyListCache.get( containerPart ) || [];
+  
+      const newKeys = [];
+      const newValues = [];
+      const newParts = [];
+  
+      for( let index = 0, item; index < items.length; index++ ){
+        item = items[ index ];
+  
+        newKeys[ index ] = keyFn( item, index, items );
+        newValues[ index ] = template( item, index, items );
       }
-      else if( oldKeys[ oldHead ] === newKeys[ newHead ] ){
-        newParts[ newHead ] = updatePart( oldParts[ oldHead ], newValues[ newHead ] );
-        oldHead++;
-        newHead++;
-      }
-      else if( oldKeys[ oldTail ] === newKeys[ newTail ] ){
-        newParts[ newTail ] = updatePart( oldParts[ oldTail ], newValues[ newTail ] );
-        oldTail--;
-        newTail--;
-      }
-      else if( oldKeys[ oldHead ] === newKeys[ newTail ] ){
-        newParts[ newTail ] = updatePart( oldParts[ oldHead ], newValues[ newTail ] );
-        insertPartBefore( containerPart, oldParts[ oldHead ], newParts[ newTail + 1 ] );
-        oldHead++;
-        newTail--;
-      }
-      else if( oldKeys[ oldTail ] === newKeys[ newHead ] ){
-        newParts[ newHead ] = updatePart( oldParts[ oldTail ], newValues[ newHead ] );
-        oldTail--;
-        newHead++;
-      }
-      else{
-        if( newKeyToIndexMap === void 0 ){
-          newKeyToIndexMap = generateMap( newKeys, newHead, newTail );
-          oldKeyToIndexMap = generateMap( oldKeys, oldHead, oldTail );
-        }
-        if( newKeyToIndexMap.has( oldKeys[ oldHead ] ) ){
-          removePart( oldParts[ oldHead ] );
+  
+      let newKeyToIndexMap;
+      let oldKeyToIndexMap;
+  
+      let oldHead = 0;
+      let oldTail = oldParts.length - 1;
+      let newHead = 0;
+      let newTail = newValues.length - 1;
+  
+  
+      while( oldHead <= oldTail && newHead <= newTail ){
+        if( oldParts[ oldHead ] === null ){
           oldHead++;
         }
-        else if( !newKeyToIndexMap.has( oldKeys[oldTail] ) ){
-          removePart( oldParts[ oldTail ] );
+        else if( oldParts[ oldTail ] === null ){
           oldTail--;
         }
-        else{
-          const oldIndex = oldKeyToIndexMap.get( newKeys[ newHead ] );
-          const oldPart = oldIndex !== void 0 ? oldParts[oldIndex] : null;
-
-          if( oldPart === null ){
-            const newPart = createAndInsertPart( containerPart, oldParts[ oldHead ] );
-
-            updatePart( newPart, newValues[ newHead ] );
-            newParts[ newHead ] = newPart;
-          }else{
-            newParts[ newHead ] = updatePart( oldPart, newValues[ newHead ] );
-            insertPartBefore(containerPart, oldPart, oldParts[oldHead]);
-            oldParts[oldIndex] = null;
-          }
-
+        else if( oldKeys[ oldHead ] === newKeys[ newHead ] ){
+          newParts[ newHead ] = updatePart( oldParts[ oldHead ], newValues[ newHead ] );
+          oldHead++;
           newHead++;
         }
+        else if( oldKeys[ oldTail ] === newKeys[ newTail ] ){
+          newParts[ newTail ] = updatePart( oldParts[ oldTail ], newValues[ newTail ] );
+          oldTail--;
+          newTail--;
+        }
+        else if( oldKeys[ oldHead ] === newKeys[ newTail ] ){
+          newParts[ newTail ] = updatePart( oldParts[ oldHead ], newValues[ newTail ] );
+          insertPartBefore( containerPart, oldParts[ oldHead ], newParts[ newTail + 1 ] );
+          oldHead++;
+          newTail--;
+        }
+        else if( oldKeys[ oldTail ] === newKeys[ newHead ] ){
+          newParts[ newHead ] = updatePart( oldParts[ oldTail ], newValues[ newHead ] );
+          oldTail--;
+          newHead++;
+        }
+        else{
+          if( newKeyToIndexMap === void 0 ){
+            newKeyToIndexMap = generateMap( newKeys, newHead, newTail );
+            oldKeyToIndexMap = generateMap( oldKeys, oldHead, oldTail );
+          }
+          if( newKeyToIndexMap.has( oldKeys[ oldHead ] ) ){
+            removePart( oldParts[ oldHead ] );
+            oldHead++;
+          }
+          else if( !newKeyToIndexMap.has( oldKeys[oldTail] ) ){
+            removePart( oldParts[ oldTail ] );
+            oldTail--;
+          }
+          else{
+            const oldIndex = oldKeyToIndexMap.get( newKeys[ newHead ] );
+            const oldPart = oldIndex !== void 0 ? oldParts[ oldIndex ] : null;
+  
+            if( oldPart === null ){
+              const newPart = createAndInsertPart( containerPart, oldParts[ oldHead ] );
+  
+              updatePart( newPart, newValues[ newHead ] );
+              newParts[ newHead ] = newPart;
+            }else{
+              newParts[ newHead ] = updatePart( oldPart, newValues[ newHead ] );
+              insertPartBefore( containerPart, oldPart, oldParts[ oldHead ] );
+              oldParts[oldIndex] = null;
+            }
+  
+            newHead++;
+          }
+        }
       }
-    }
-
-    while( newHead <= newTail ){
-      const newPart = createAndInsertPart( containerPart, newParts[ newTail + 1 ] );
-      updatePart( newPart, newValues[ newHead ] );
-      newParts[ newHead++ ] = newPart;
-    }
-
-    while( oldHead <= oldTail ){
-      const oldPart = oldParts[ oldHead++ ];
-      
-      if ( oldPart !== null ){
-        removePart( oldPart );
+  
+      while( newHead <= newTail ){
+        const newPart = createAndInsertPart( containerPart, newParts[ newTail + 1 ] );
+        updatePart( newPart, newValues[ newHead ] );
+        newParts[ newHead++ ] = newPart;
       }
-    }
+  
+      while( oldHead <= oldTail ){
+        const oldPart = oldParts[ oldHead++ ];
+        
+        if ( oldPart !== null ){
+          removePart( oldPart );
+        }
+      }
+  
+      partListCache.set( containerPart, newParts );
+      keyListCache.set( containerPart, newKeys );
+    },
+    /**
+     * destroy
+     */
+    containerPart => {
+      const parts = partListCache.get( containerPart );
 
-    partListCache.set( containerPart, newParts );
-    keyListCache.set( containerPart, newKeys );
-  };
+      if( parts ) parts.forEach( part => {
+        return destroyPart( part );
+      });
+    }
+  ];
 });
 
 
@@ -155,6 +171,7 @@ function generateMap( list, start, end ){
 }
 
 function removePart( part ){
+  console.log( 123 );
   removeNodes( part.startNode.parentNode, part.startNode, part.endNode.nextSibling );
 }
 
--- test/Lib/hu.js
+++ test/Lib/hu.js
@@ -3234,112 +3234,127 @@
   var repeat = directiveFn(( items, key, template ) => {
     const keyFn = isFunction( key ) ? key : item => item[ key ];
 
-    return containerPart => {
-      if( !( containerPart instanceof NodePart ) ){
-        throw new Error('Hu.html.repeat 指令方法只能在文本区域中使用 !');
-      }
-
-      const oldParts = partListCache.get( containerPart ) || [];
-      const oldKeys = keyListCache.get( containerPart ) || [];
-
-      const newKeys = [];
-      const newValues = [];
-      const newParts = [];
-
-      for( let index = 0, item; index < items.length; index++ ){
-        item = items[ index ];
-
-        newKeys[ index ] = keyFn( item, index, items );
-        newValues[ index ] = template( item, index, items );
-      }
-
-      let newKeyToIndexMap;
-      let oldKeyToIndexMap;
-
-      let oldHead = 0;
-      let oldTail = oldParts.length - 1;
-      let newHead = 0;
-      let newTail = newValues.length - 1;
-
-
-      while( oldHead <= oldTail && newHead <= newTail ){
-        if( oldParts[ oldHead ] === null ){
-          oldHead++;
+    return [
+      /**
+       * commit
+       */
+      containerPart => {
+        if( !( containerPart instanceof NodePart ) ){
+          throw new Error('Hu.html.repeat 指令方法只能在文本区域中使用 !');
         }
-        else if( oldParts[ oldTail ] === null ){
-          oldTail--;
+    
+        const oldParts = partListCache.get( containerPart ) || [];
+        const oldKeys = keyListCache.get( containerPart ) || [];
+    
+        const newKeys = [];
+        const newValues = [];
+        const newParts = [];
+    
+        for( let index = 0, item; index < items.length; index++ ){
+          item = items[ index ];
+    
+          newKeys[ index ] = keyFn( item, index, items );
+          newValues[ index ] = template( item, index, items );
         }
-        else if( oldKeys[ oldHead ] === newKeys[ newHead ] ){
-          newParts[ newHead ] = updatePart( oldParts[ oldHead ], newValues[ newHead ] );
-          oldHead++;
-          newHead++;
-        }
-        else if( oldKeys[ oldTail ] === newKeys[ newTail ] ){
-          newParts[ newTail ] = updatePart( oldParts[ oldTail ], newValues[ newTail ] );
-          oldTail--;
-          newTail--;
-        }
-        else if( oldKeys[ oldHead ] === newKeys[ newTail ] ){
-          newParts[ newTail ] = updatePart( oldParts[ oldHead ], newValues[ newTail ] );
-          insertPartBefore( containerPart, oldParts[ oldHead ], newParts[ newTail + 1 ] );
-          oldHead++;
-          newTail--;
-        }
-        else if( oldKeys[ oldTail ] === newKeys[ newHead ] ){
-          newParts[ newHead ] = updatePart( oldParts[ oldTail ], newValues[ newHead ] );
-          oldTail--;
-          newHead++;
-        }
-        else{
-          if( newKeyToIndexMap === void 0 ){
-            newKeyToIndexMap = generateMap( newKeys, newHead, newTail );
-            oldKeyToIndexMap = generateMap( oldKeys, oldHead, oldTail );
-          }
-          if( newKeyToIndexMap.has( oldKeys[ oldHead ] ) ){
-            removePart( oldParts[ oldHead ] );
+    
+        let newKeyToIndexMap;
+        let oldKeyToIndexMap;
+    
+        let oldHead = 0;
+        let oldTail = oldParts.length - 1;
+        let newHead = 0;
+        let newTail = newValues.length - 1;
+    
+    
+        while( oldHead <= oldTail && newHead <= newTail ){
+          if( oldParts[ oldHead ] === null ){
             oldHead++;
           }
-          else if( !newKeyToIndexMap.has( oldKeys[oldTail] ) ){
-            removePart( oldParts[ oldTail ] );
+          else if( oldParts[ oldTail ] === null ){
             oldTail--;
           }
-          else{
-            const oldIndex = oldKeyToIndexMap.get( newKeys[ newHead ] );
-            const oldPart = oldIndex !== void 0 ? oldParts[oldIndex] : null;
-
-            if( oldPart === null ){
-              const newPart = createAndInsertPart( containerPart, oldParts[ oldHead ] );
-
-              updatePart( newPart, newValues[ newHead ] );
-              newParts[ newHead ] = newPart;
-            }else{
-              newParts[ newHead ] = updatePart( oldPart, newValues[ newHead ] );
-              insertPartBefore(containerPart, oldPart, oldParts[oldHead]);
-              oldParts[oldIndex] = null;
-            }
-
+          else if( oldKeys[ oldHead ] === newKeys[ newHead ] ){
+            newParts[ newHead ] = updatePart( oldParts[ oldHead ], newValues[ newHead ] );
+            oldHead++;
             newHead++;
           }
+          else if( oldKeys[ oldTail ] === newKeys[ newTail ] ){
+            newParts[ newTail ] = updatePart( oldParts[ oldTail ], newValues[ newTail ] );
+            oldTail--;
+            newTail--;
+          }
+          else if( oldKeys[ oldHead ] === newKeys[ newTail ] ){
+            newParts[ newTail ] = updatePart( oldParts[ oldHead ], newValues[ newTail ] );
+            insertPartBefore( containerPart, oldParts[ oldHead ], newParts[ newTail + 1 ] );
+            oldHead++;
+            newTail--;
+          }
+          else if( oldKeys[ oldTail ] === newKeys[ newHead ] ){
+            newParts[ newHead ] = updatePart( oldParts[ oldTail ], newValues[ newHead ] );
+            oldTail--;
+            newHead++;
+          }
+          else{
+            if( newKeyToIndexMap === void 0 ){
+              newKeyToIndexMap = generateMap( newKeys, newHead, newTail );
+              oldKeyToIndexMap = generateMap( oldKeys, oldHead, oldTail );
+            }
+            if( newKeyToIndexMap.has( oldKeys[ oldHead ] ) ){
+              removePart( oldParts[ oldHead ] );
+              oldHead++;
+            }
+            else if( !newKeyToIndexMap.has( oldKeys[oldTail] ) ){
+              removePart( oldParts[ oldTail ] );
+              oldTail--;
+            }
+            else{
+              const oldIndex = oldKeyToIndexMap.get( newKeys[ newHead ] );
+              const oldPart = oldIndex !== void 0 ? oldParts[ oldIndex ] : null;
+    
+              if( oldPart === null ){
+                const newPart = createAndInsertPart( containerPart, oldParts[ oldHead ] );
+    
+                updatePart( newPart, newValues[ newHead ] );
+                newParts[ newHead ] = newPart;
+              }else{
+                newParts[ newHead ] = updatePart( oldPart, newValues[ newHead ] );
+                insertPartBefore( containerPart, oldPart, oldParts[ oldHead ] );
+                oldParts[oldIndex] = null;
+              }
+    
+              newHead++;
+            }
+          }
         }
-      }
-
-      while( newHead <= newTail ){
-        const newPart = createAndInsertPart( containerPart, newParts[ newTail + 1 ] );
-        updatePart( newPart, newValues[ newHead ] );
-        newParts[ newHead++ ] = newPart;
-      }
-
-      while( oldHead <= oldTail ){
-        const oldPart = oldParts[ oldHead++ ];
-        
-        if ( oldPart !== null ){
-          removePart( oldPart );
+    
+        while( newHead <= newTail ){
+          const newPart = createAndInsertPart( containerPart, newParts[ newTail + 1 ] );
+          updatePart( newPart, newValues[ newHead ] );
+          newParts[ newHead++ ] = newPart;
         }
-      }
+    
+        while( oldHead <= oldTail ){
+          const oldPart = oldParts[ oldHead++ ];
+          
+          if ( oldPart !== null ){
+            removePart( oldPart );
+          }
+        }
+    
+        partListCache.set( containerPart, newParts );
+        keyListCache.set( containerPart, newKeys );
+      },
+      /**
+       * destroy
+       */
+      containerPart => {
+        const parts = partListCache.get( containerPart );
 
-      partListCache.set( containerPart, newParts );
-      keyListCache.set( containerPart, newKeys );
-    };
+        if( parts ) parts.forEach( part => {
+          return destroyPart( part );
+        });
+      }
+    ];
   });
 
 
@@ -3367,6 +3382,7 @@
   }
 
   function removePart( part ){
+    console.log( 123 );
     removeNodes( part.startNode.parentNode, part.startNode, part.endNode.nextSibling );
   }
 
@@ -3892,7 +3908,7 @@
     removeComputed( computedMap, this );
     removeComputed( watcherMap, this );
 
-    // 注销
+    // 注销 render 时创建的指令及指令方法
     destroyDirective( this.$el );
 
     // 清空 render 方法收集到的依赖
