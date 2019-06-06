d197a977b26a9f1139ac8b41794c7d2a190dae02
Zhang-Wei-666 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-06-06 18:34:35 +0800

    对工具方法 `triggerEvent` 进行了重构

--- src/shared/util/triggerEvent.js
+++ src/shared/util/triggerEvent.js
@@ -1,11 +1,31 @@
+import { isArray } from "../global/Array/index";
+
 export default
 /**
  * 触发事件
- * @param {Element} elem
- * @param {string} type
+ * @param {Element} elem 触发事件的元素对象
+ * @param {string} type 事件名称
+ * @param {Function} process 其它处理
  */
-( target, type ) => {
+( target, type, process ) => {
+  /**
+   * 创建事件对象
+   */
   const event = document.createEvent('HTMLEvents');
-  event.initEvent( type, true, true );
+
+  // 如果想设置 initEvent 方法的 bubbles, cancelable 参数
+  // 可以将 type 替换为数组
+  // 数组内依次是 type, bubbles, cancelable
+  if( !isArray( type ) ){
+    type = [ type, true, true ];
+  }
+
+  // 初始化事件对象
+  event.initEvent( ...type );
+
+  // 可传入方法对事件对象做其它处理
+  if( process ) process( event, target );
+
+  // 触发事件
   target.dispatchEvent( event );
 }
