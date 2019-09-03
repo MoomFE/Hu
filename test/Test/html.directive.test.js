a8d6bc07756bfc621a27b8feef7d7f38f8c5d1a0
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-09-03 16:18:25 +0800

    完善单元测试

--- test/Test/html.directive.test.js
+++ test/Test/html.directive.test.js
@@ -1,7 +1,154 @@
 describe( 'html.directive', () => {
 
-  it( '', () => {
-    
+  const render = Hu.render;
+  const html = Hu.html;
+
+  /** @type {Element} */
+  let div;
+  beforeEach(() => {
+    div = document.createElement('div');
+  });
+  afterEach(() => {
+    div.$remove();
+  });
+
+
+  it( '正常对元素属性 ( Attribute ) 进行绑定', () => {
+    render( div )`
+      <div name=${ 123 }></div>
+    `;
+    expect( stripExpressionMarkers( div.innerHTML ) ).is.equals(`
+      <div name="123"></div>
+    `);
+
+    render( div )`
+      <div style="width: ${ 100 }px; height: ${ 200 }px; opacity: ${ 0.5 }"></div>
+    `;
+    expect( div.firstElementChild.style ).is.include({
+      width: '100px',
+      height: '200px',
+      opacity: '0.5'
+    });
+  });
+
+  it( '使用 :class 指令对元素 className 进行绑定 ( 字符串方式 )', () => {
+    const div = document.createElement('div');
+
+    // 1
+    Hu.render( div )`
+      <div :class=${ 'a b c' }></div>
+    `;
+
+    expect(
+      Array.from( div.firstElementChild.classList )
+    ).is.deep.equals([ 'a', 'b', 'c' ]);
+
+    // 2
+    Hu.render( div )`
+      <div class="a" :class=${ 'b c' }></div>
+    `;
+
+    expect(
+      Array.from( div.firstElementChild.classList )
+    ).is.deep.equals([ 'a', 'b', 'c' ]);
+  });
+
+  it( '使用 :class 指令对元素 className 进行绑定 ( JSON 方式 )', () => {
+    const div = document.createElement('div');
+
+    // 1
+    Hu.render( div )`
+      <div :class=${{ a: true, b: true, c: true }}></div>
+    `;
+
+    expect(
+      Array.from( div.firstElementChild.classList )
+    ).is.deep.equals([ 'a', 'b', 'c' ]);
+
+    // 2
+    Hu.render( div )`
+      <div :class=${{ a: true, b: false, c: true }}></div>
+    `;
+
+    expect(
+      Array.from( div.firstElementChild.classList )
+    ).is.deep.equals([ 'a', 'c' ]);
+
+    // 3
+    Hu.render( div )`
+      <div class="a" :class=${{ b: true, c: true }}></div>
+    `;
+
+    expect(
+      Array.from( div.firstElementChild.classList )
+    ).is.deep.equals([ 'a', 'b', 'c' ]);
+
+    // 4
+    Hu.render( div )`
+      <div class="a" :class=${{ b: false, c: true }}></div>
+    `;
+
+    expect(
+      Array.from( div.firstElementChild.classList )
+    ).is.deep.equals([ 'a', 'c' ]);
+  });
+
+  it( '使用 :class 指令对元素 className 进行绑定 ( 数组方式 )', () => {
+    const div = document.createElement('div');
+
+    // 1
+    Hu.render( div )`
+      <div :class=${[ 'a', 'b', 'c' ]}></div>
+    `;
+
+    expect(
+      Array.from( div.firstElementChild.classList )
+    ).is.deep.equals([ 'a', 'b', 'c' ]);
+
+    // 2
+    Hu.render( div )`
+      <div :class=${[ { a: true }, 'b', { c: true } ]}></div>
+    `;
+
+    expect(
+      Array.from( div.firstElementChild.classList )
+    ).is.deep.equals([ 'a', 'b', 'c' ]);
+
+    // 3
+    Hu.render( div )`
+      <div :class=${[ 'a b', { c: false } ]}></div>
+    `;
+
+    expect(
+      Array.from( div.firstElementChild.classList )
+    ).is.deep.equals([ 'a', 'b' ]);
+
+    // 4
+    Hu.render( div )`
+      <div class="a" :class=${[ 'b', 'c' ]}></div>
+    `;
+
+    expect(
+      Array.from( div.firstElementChild.classList )
+    ).is.deep.equals([ 'a', 'b', 'c' ]);
+
+    // 5
+    Hu.render( div )`
+      <div class="a" :class=${[ 'b', { c: true } ]}></div>
+    `;
+
+    expect(
+      Array.from( div.firstElementChild.classList )
+    ).is.deep.equals([ 'a', 'b', 'c' ]);
+
+    // 6
+    Hu.render( div )`
+      <div class="a" :class=${[ 'b', { c: false } ]}></div>
+    `;
+
+    expect(
+      Array.from( div.firstElementChild.classList )
+    ).is.deep.equals([ 'a', 'b' ]);
   });
 
 });
