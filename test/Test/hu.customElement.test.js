1485575300e3a8b62c4b5d169e91f4275e3c8ff8
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-09-05 18:21:20 +0800

    完善单元测试

---
+++ test/Test/hu.customElement.test.js
@@ -0,0 +1,28 @@
+describe( 'hu.customElement', () => {
+
+  it( '自定义元素上的 $hu 属性为当前自定义元素的 Hu 实例', () => {
+    const customName = window.customName;
+
+    Hu.define( customName );
+
+    const custom = document.createElement( customName );
+    const hu = custom.$hu;
+
+    expect( hu ).is.instanceOf( Hu );
+    expect( hu.$customElement ).is.equals( custom );
+  });
+
+  it( '自定义元素上的 $on, $once, $off 方法为当前自定义元素的 Hu 实例上 $on 方法的映射', () => {
+    const customName = window.customName;
+
+    Hu.define( customName );
+
+    const custom = document.createElement( customName );
+    const hu = custom.$hu;
+
+    expect( custom.$on ).is.equals( hu.$on );
+    expect( custom.$off ).is.equals( hu.$off );
+    expect( custom.$once ).is.equals( hu.$once );
+  });
+
+});
