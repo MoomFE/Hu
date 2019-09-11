8a3187a1398cfbaad90a24f2a4f88275720556c8
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-09-11 14:36:23 +0800

    完善单元测试

--- test/Test/hu.customElement.test.js
+++ test/Test/hu.customElement.test.js
@@ -12,7 +12,7 @@ describe( 'hu.customElement', () => {
     expect( hu.$customElement ).is.equals( custom );
   });
 
-  it( '自定义元素上的 $on, $once, $off 方法为当前自定义元素的 Hu 实例上 $on, $once, $off 方法的映射', () => {
+  it( '自定义元素上的 $on 方法为当前自定义元素的 Hu 实例上 $on 方法的映射', () => {
     const customName = window.customName;
 
     Hu.define( customName );
@@ -20,12 +20,38 @@ describe( 'hu.customElement', () => {
     const custom = document.createElement( customName );
     const hu = custom.$hu;
 
-    expect( custom.$on ).is.equals( hu.$on );
-    expect( custom.$off ).is.equals( hu.$off );
-    expect( custom.$once ).is.equals( hu.$once );
+    let index = 0;
+    let result, result1;
+
+    custom.$on( 'test', function(){
+      index++;
+      result = [ ...arguments ];
+      expect( hu ).is.equals( this );
+    });
+
+    custom.$on([ 'test1', 'test2' ], function(){
+      index++;
+      result1 = [ ...arguments ];
+      expect( hu ).is.equals( this );
+    });
+
+    hu.$emit( 'test', 1, 2, 3 );
+    expect( index ).is.equals( 1 );
+    expect( result ).is.deep.equals([ 1, 2, 3 ]);
+    expect( result1 ).is.undefined;
+
+    hu.$emit( 'test1', 4, 5, 6 );
+    expect( index ).is.equals( 2 );
+    expect( result ).is.deep.equals([ 1, 2, 3 ]);
+    expect( result1 ).is.deep.equals([ 4, 5, 6 ]);
+
+    hu.$emit( 'test2', 7, 8, 9 );
+    expect( index ).is.equals( 3 );
+    expect( result ).is.deep.equals([ 1, 2, 3 ]);
+    expect( result1 ).is.deep.equals([ 7, 8, 9 ]);
   });
 
-  it( '自定义元素上的 addEventListener, removeEventListener 方法为当前自定义元素的 Hu 实例上 $on, $off 方法的映射', () => {
+  it( '自定义元素上的 $once 方法为当前自定义元素的 Hu 实例上 $once 方法的映射', () => {
     const customName = window.customName;
 
     Hu.define( customName );
@@ -33,8 +59,201 @@ describe( 'hu.customElement', () => {
     const custom = document.createElement( customName );
     const hu = custom.$hu;
 
-    expect( custom.addEventListener ).is.equals( hu.$on );
-    expect( custom.removeEventListener ).is.equals( hu.$off );
+    let index = 0;
+    let result, result1;
+
+    custom.$once( 'test', function(){
+      index++;
+      result = [ ...arguments ];
+      expect( hu ).is.equals( this );
+    });
+
+    hu.$on( 'test1', function(){
+      index++;
+      result1 = [ ...arguments ];
+      expect( hu ).is.equals( this );
+    });
+
+    hu.$emit( 'test', 1, 2, 3 );
+    expect( index ).is.equals( 1 );
+    expect( result ).is.deep.equals([ 1, 2, 3 ]);
+    expect( result1 ).is.undefined;
+
+    hu.$emit( 'test', 1, 2, 3 );
+    expect( index ).is.equals( 1 );
+    expect( result ).is.deep.equals([ 1, 2, 3 ]);
+    expect( result1 ).is.undefined;
+
+    hu.$emit( 'test1', 4, 5, 6 );
+    expect( index ).is.equals( 2 );
+    expect( result ).is.deep.equals([ 1, 2, 3 ]);
+    expect( result1 ).is.deep.equals([ 4, 5, 6 ]);
+
+    hu.$emit( 'test1', 7, 8, 9 );
+    expect( index ).is.equals( 3 );
+    expect( result ).is.deep.equals([ 1, 2, 3 ]);
+    expect( result1 ).is.deep.equals([ 7, 8, 9 ]);
+  });
+
+  it( '自定义元素上的 $off 方法为当前自定义元素的 Hu 实例上 $off 方法的映射', () => {
+    const customName = window.customName;
+
+    Hu.define( customName );
+
+    const custom = document.createElement( customName );
+    const hu = custom.$hu;
+
+    let index = 0;
+    let result, result1;
+
+    function fn(){
+      index++;
+      result = [ ...arguments ];
+    }
+
+    function fn1(){
+      index++;
+      result1 = [ ...arguments ];
+    }
+
+    hu.$on( [ 'test', 'test1' ], fn );
+    hu.$on( [ 'test', 'test1' ], fn1 );
+
+    // 解绑某个事件的某个回调
+    hu.$emit( 'test', 1, 2, 3 );
+    expect( index ).is.equals( 2 );
+    expect( result ).is.deep.equals([ 1, 2, 3 ]);
+    expect( result1 ).is.deep.equals([ 1, 2, 3 ]);
+
+    custom.$off( 'test', fn );
+    hu.$emit( 'test', 4, 5, 6 );
+    expect( index ).is.equals( 3 );
+    expect( result ).is.deep.equals([ 1, 2, 3 ]);
+    expect( result1 ).is.deep.equals([ 4, 5, 6 ]);
+
+    // 解绑某个事件的全部回调
+    hu.$emit( 'test1', 7, 8, 9 );
+    expect( index ).is.equals( 5 );
+    expect( result ).is.deep.equals([ 7, 8, 9 ]);
+    expect( result1 ).is.deep.equals([ 7, 8, 9 ]);
+
+    custom.$off( 'test1' );
+    hu.$emit( 'test1', 1, 2, 3 );
+    expect( index ).is.equals( 5 );
+    expect( result ).is.deep.equals([ 7, 8, 9 ]);
+    expect( result1 ).is.deep.equals([ 7, 8, 9 ]);
+
+    // 解绑所有事件
+    hu.$emit( 'test', 4, 5, 6 );
+    expect( index ).is.equals( 6 );
+    expect( result ).is.deep.equals([ 7, 8, 9 ]);
+    expect( result1 ).is.deep.equals([ 4, 5, 6 ]);
+
+    custom.$off();
+    hu.$emit( 'test', 1, 2, 3 );
+    expect( index ).is.equals( 6 );
+    expect( result ).is.deep.equals([ 7, 8, 9 ]);
+    expect( result1 ).is.deep.equals([ 4, 5, 6 ]);
+  });
+
+  it( '自定义元素上的 addEventListener 方法为当前自定义元素的 Hu 实例上 $on 方法的映射', () => {
+    const customName = window.customName;
+
+    Hu.define( customName );
+
+    const custom = document.createElement( customName );
+    const hu = custom.$hu;
+
+    let index = 0;
+    let result, result1;
+
+    custom.addEventListener( 'test', function(){
+      index++;
+      result = [ ...arguments ];
+      expect( hu ).is.equals( this );
+    });
+
+    custom.addEventListener([ 'test1', 'test2' ], function(){
+      index++;
+      result1 = [ ...arguments ];
+      expect( hu ).is.equals( this );
+    });
+
+    hu.$emit( 'test', 1, 2, 3 );
+    expect( index ).is.equals( 1 );
+    expect( result ).is.deep.equals([ 1, 2, 3 ]);
+    expect( result1 ).is.undefined;
+
+    hu.$emit( 'test1', 4, 5, 6 );
+    expect( index ).is.equals( 2 );
+    expect( result ).is.deep.equals([ 1, 2, 3 ]);
+    expect( result1 ).is.deep.equals([ 4, 5, 6 ]);
+
+    hu.$emit( 'test2', 7, 8, 9 );
+    expect( index ).is.equals( 3 );
+    expect( result ).is.deep.equals([ 1, 2, 3 ]);
+    expect( result1 ).is.deep.equals([ 7, 8, 9 ]);
+  });
+
+  it( '自定义元素上的 removeEventListener 方法为当前自定义元素的 Hu 实例上 $off 方法的映射', () => {
+    const customName = window.customName;
+
+    Hu.define( customName );
+
+    const custom = document.createElement( customName );
+    const hu = custom.$hu;
+
+    let index = 0;
+    let result, result1;
+
+    function fn(){
+      index++;
+      result = [ ...arguments ];
+    }
+
+    function fn1(){
+      index++;
+      result1 = [ ...arguments ];
+    }
+
+    hu.$on( [ 'test', 'test1' ], fn );
+    hu.$on( [ 'test', 'test1' ], fn1 );
+
+    // 解绑某个事件的某个回调
+    hu.$emit( 'test', 1, 2, 3 );
+    expect( index ).is.equals( 2 );
+    expect( result ).is.deep.equals([ 1, 2, 3 ]);
+    expect( result1 ).is.deep.equals([ 1, 2, 3 ]);
+
+    custom.removeEventListener( 'test', fn );
+    hu.$emit( 'test', 4, 5, 6 );
+    expect( index ).is.equals( 3 );
+    expect( result ).is.deep.equals([ 1, 2, 3 ]);
+    expect( result1 ).is.deep.equals([ 4, 5, 6 ]);
+
+    // 解绑某个事件的全部回调
+    hu.$emit( 'test1', 7, 8, 9 );
+    expect( index ).is.equals( 5 );
+    expect( result ).is.deep.equals([ 7, 8, 9 ]);
+    expect( result1 ).is.deep.equals([ 7, 8, 9 ]);
+
+    custom.removeEventListener( 'test1' );
+    hu.$emit( 'test1', 1, 2, 3 );
+    expect( index ).is.equals( 5 );
+    expect( result ).is.deep.equals([ 7, 8, 9 ]);
+    expect( result1 ).is.deep.equals([ 7, 8, 9 ]);
+
+    // 解绑所有事件
+    hu.$emit( 'test', 4, 5, 6 );
+    expect( index ).is.equals( 6 );
+    expect( result ).is.deep.equals([ 7, 8, 9 ]);
+    expect( result1 ).is.deep.equals([ 4, 5, 6 ]);
+
+    custom.$off();
+    hu.$emit( 'test', 1, 2, 3 );
+    expect( index ).is.equals( 6 );
+    expect( result ).is.deep.equals([ 7, 8, 9 ]);
+    expect( result1 ).is.deep.equals([ 4, 5, 6 ]);
   });
 
 });
