2883de0816c0ba681716b6478117e36934ddd510
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-09-16 11:08:06 +0800

    细节调整

--- src/static/define/util/injectionPrivateToInstance.js
+++ src/static/define/util/injectionPrivateToInstance.js
@@ -6,19 +6,17 @@ export default
 /**
  * 在实例和自定义元素上建立内部对象的引用
  */
-( isCustomElement, target, root, data ) => {
-  each( data, ( key, value ) => {
+( isCustomElement, target, root, data ) => each( data, ( key, value ) => {
 
-    // 实例上直接写入就好
-    // 常规操作有观察者对象进行拦截
-    target[ key ] = value;
+  // 实例上直接写入就好
+  // 常规操作有观察者对象进行拦截
+  target[ key ] = value;
 
-    // 自定义元素上需要通过 defineProperty 进行转发
-    if( isCustomElement ){
-      defineProperty( root, key, {
-        value
-      });
-    }
+  // 自定义元素上需要通过 defineProperty 进行转发
+  if( isCustomElement ){
+    defineProperty( root, key, {
+      value
+    });
+  }
 
-  });
-}
+});
