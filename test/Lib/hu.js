7f521be35cbb9f8ea5f7cb691bb6023038a95d77
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-09-26 15:52:55 +0800

    :star2: 1.0.0-bata.19

--- test/Lib/hu.js
+++ test/Lib/hu.js
@@ -1,5 +1,5 @@
 /*!
- * Hu.js v1.0.0-bata.18
+ * Hu.js v1.0.0-bata.19
  * https://github.com/MoomFE/Hu
  * 
  * (c) 2018-present Wei Zhang
@@ -4349,7 +4349,7 @@
     }
   });
 
-  Hu$1.version = '1.0.0-bata.18';
+  Hu$1.version = '1.0.0-bata.19';
 
   var initAttributeChangedCallback = propsMap => function( name, oldValue, value ){
     if( value === oldValue ) return;
