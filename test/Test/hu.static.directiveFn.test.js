970d74a08a0a3106c959b07e1c4f69c2396143ac
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-12-10 14:59:34 +0800

    完善单元测试

--- test/Test/hu.static.directiveFn.test.js
+++ test/Test/hu.static.directiveFn.test.js
@@ -231,523 +231,129 @@ describe( 'Hu.static.directiveFn', () => {
     expect( result ).is.equals( 11 );
   });
 
-  // it( 'Hu.directiveFn: 注册的指令方法在不同地方的使用 ( 在 render 中使用 )', () => {
-  //   let result;
-  //   const fn = Hu.directiveFn( class{
-  //     commit( value ){
-  //       result = value;
-  //     }
-  //   });
-
-  //   Hu.render(
-  //     fn( 123 ),
-  //     div
-  //   );
-  //   expect( result ).is.equals( 123 );
-  // });
-
-  // it( 'Hu.directiveFn: 注册的指令方法在不同地方的使用 ( 在指令中使用 )', () => {
-  //   let result;
-  //   const fn = Hu.directiveFn( class{
-  //     commit( value ){
-  //       result = value;
-  //     }
-  //   });
-
-  //   Hu.render( div )`
-  //     <div :text=${ fn( 123 ) }></div>
-  //   `;
-  //   expect( result ).is.equals( 123 );
-  // });
-
-  // it( 'Hu.directiveFn: 注册的指令方法在不同地方的使用 ( 在模板中使用 )', () => {
-  //   let result;
-  //   const fn = Hu.directiveFn( class{
-  //     commit( value ){
-  //       result = value;
-  //     }
-  //   });
-
-  //   Hu.render( div )`
-  //     <div>${ fn( 123 ) }</div>
-  //   `;
-  //   expect( result ).is.equals( 123 );
-  // });
-
-  // it( 'Hu.directiveFn: 注册的指令方法在不同地方的使用 ( 在数组中使用 )', () => {
-  //   let result;
-  //   const fn = Hu.directiveFn( class{
-  //     commit( value ){
-  //       result = value;
-  //     }
-  //   });
-
-  //   Hu.render( div )`
-  //     <div>${[ fn( 123 ) ]}</div>
-  //   `;
-  //   expect( result ).is.equals( 123 );
-  // });
-
-  // it( 'Hu.directiveFn: 注册的指令方法在不同地方的使用 ( 在 repeat 指令方法中使用 )', () => {
-  //   let result;
-  //   const fn = Hu.directiveFn( class{
-  //     commit( value ){
-  //       result = value;
-  //     }
-  //   });
-
-  //   Hu.render( div )`
-  //     <div>${
-  //       Hu.html.repeat( [ 123 ], val => val, val => {
-  //         return fn( val );
-  //       })
-  //     }</div>
-  //   `;
-  //   expect( result ).is.equals( 123 );
-  // });
-
-  // it( 'Hu.directiveFn: 注册的指令方法在被弃用时会触发对应 destroy 方法 ( 在 render 中使用 )', () => {
-  //   let commitIndex = 0;
-  //   let destroyIndex = 0;
-  //   const fn = Hu.directiveFn( class{
-  //     commit( value ){
-  //       commitIndex++;
-  //     }
-  //     destroy(){
-  //       destroyIndex++;
-  //     }
-  //   });
-
-  //   expect( commitIndex ).is.equals( 0 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render(
-  //     fn( 123 ),
-  //     div
-  //   );
-  //   expect( commitIndex ).is.equals( 1 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render(
-  //     123,
-  //     div
-  //   );
-  //   expect( commitIndex ).is.equals( 1 );
-  //   expect( destroyIndex ).is.equals( 1 );
-
-  //   // ------
-
-  //   Hu.render(
-  //     fn( 456 ),
-  //     div
-  //   );
-  //   expect( commitIndex ).is.equals( 2 );
-  //   expect( destroyIndex ).is.equals( 1 );
-
-  //   Hu.render(
-  //     456,
-  //     div
-  //   );
-  //   expect( commitIndex ).is.equals( 2 );
-  //   expect( destroyIndex ).is.equals( 2 );
-  // });
-
-  // it( 'Hu.directiveFn: 注册的指令方法在被弃用时会触发对应 destroy 方法 ( 在指令中使用 )', () => {
-  //   let commitIndex = 0;
-  //   let destroyIndex = 0;
-  //   const fn = Hu.directiveFn( class{
-  //     commit( value ){
-  //       commitIndex++;
-  //     }
-  //     destroy(){
-  //       destroyIndex++;
-  //     }
-  //   });
-
-  //   expect( commitIndex ).is.equals( 0 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div :text=${ fn( 123 ) }></div>
-  //   `;
-  //   expect( commitIndex ).is.equals( 1 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div></div>
-  //   `;
-  //   expect( commitIndex ).is.equals( 1 );
-  //   expect( destroyIndex ).is.equals( 1 );
-
-  //   // ------
-
-  //   Hu.render(
-  //     Hu.html`
-  //       <div :text=${ fn( 456 ) }></div>
-  //     `,
-  //     div
-  //   );
-  //   expect( commitIndex ).is.equals( 2 );
-  //   expect( destroyIndex ).is.equals( 1 );
-
-  //   Hu.render(
-  //     Hu.html`
-  //       <div></div>
-  //     `,
-  //     div
-  //   );
-  //   expect( commitIndex ).is.equals( 2 );
-  //   expect( destroyIndex ).is.equals( 2 );
-  // });
-
-  // it( 'Hu.directiveFn: 注册的指令方法在被弃用时会触发对应 destroy 方法 ( 在模板中使用 )', () => {
-  //   let commitIndex = 0;
-  //   let destroyIndex = 0;
-  //   const fn = Hu.directiveFn( class{
-  //     commit( value ){
-  //       commitIndex++;
-  //     }
-  //     destroy(){
-  //       destroyIndex++;
-  //     }
-  //   });
-
-  //   expect( commitIndex ).is.equals( 0 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div>${ fn( 123 ) }</div>
-  //   `;
-  //   expect( commitIndex ).is.equals( 1 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div></div>
-  //   `;
-  //   expect( commitIndex ).is.equals( 1 );
-  //   expect( destroyIndex ).is.equals( 1 );
-
-  //   // ------
-
-  //   Hu.render(
-  //     Hu.html`
-  //       <div>${ fn( 456 ) }</div>
-  //     `,
-  //     div
-  //   );
-  //   expect( commitIndex ).is.equals( 2 );
-  //   expect( destroyIndex ).is.equals( 1 );
-
-  //   Hu.render(
-  //     Hu.html`
-  //       <div></div>
-  //     `,
-  //     div
-  //   );
-  //   expect( commitIndex ).is.equals( 2 );
-  //   expect( destroyIndex ).is.equals( 2 );
-  // });
-
-  // it( 'Hu.directiveFn: 注册的指令方法在被弃用时会触发对应 destroy 方法 ( 在数组中使用 )', () => {
-  //   let commitIndex = 0;
-  //   let destroyIndex = 0;
-  //   const fn = Hu.directiveFn( class{
-  //     commit( value ){
-  //       commitIndex++;
-  //     }
-  //     destroy(){
-  //       destroyIndex++;
-  //     }
-  //   });
-
-  //   expect( commitIndex ).is.equals( 0 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div>${[ fn( 123 ) ]}</div>
-  //   `;
-  //   expect( commitIndex ).is.equals( 1 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div></div>
-  //   `;
-  //   expect( commitIndex ).is.equals( 1 );
-  //   expect( destroyIndex ).is.equals( 1 );
-
-  //   // ------
-
-  //   Hu.render(
-  //     Hu.html`
-  //       <div>${[ fn( 456 ) ]}</div>
-  //     `,
-  //     div
-  //   );
-  //   expect( commitIndex ).is.equals( 2 );
-  //   expect( destroyIndex ).is.equals( 1 );
-
-  //   Hu.render(
-  //     Hu.html`
-  //       <div></div>
-  //     `,
-  //     div
-  //   );
-  //   expect( commitIndex ).is.equals( 2 );
-  //   expect( destroyIndex ).is.equals( 2 );
-  // });
-
-  // it( 'Hu.directiveFn: 注册的指令方法在被弃用时会触发对应 destroy 方法 ( 在 repeat 指令方法中使用 )', () => {
-  //   let commitIndex = 0;
-  //   let destroyIndex = 0;
-  //   const fn = Hu.directiveFn( class{
-  //     commit( value ){
-  //       commitIndex++;
-  //     }
-  //     destroy(){
-  //       destroyIndex++;
-  //     }
-  //   });
-
-  //   expect( commitIndex ).is.equals( 0 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div>${
-  //       Hu.html.repeat( [ 123 ], val => val, val => {
-  //         return fn( val );
-  //       })
-  //     }</div>
-  //   `;
-  //   expect( commitIndex ).is.equals( 1 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div></div>
-  //   `;
-  //   expect( commitIndex ).is.equals( 1 );
-  //   expect( destroyIndex ).is.equals( 1 );
-
-  //   // ------
-
-  //   Hu.render(
-  //     Hu.html`
-  //       <div>${
-  //         Hu.html.repeat( [ 123 ], val => val, val => {
-  //           return fn( val );
-  //         })
-  //       }</div>
-  //     `,
-  //     div
-  //   );
-  //   expect( commitIndex ).is.equals( 2 );
-  //   expect( destroyIndex ).is.equals( 1 );
-
-  //   Hu.render(
-  //     Hu.html`
-  //       <div></div>
-  //     `,
-  //     div
-  //   );
-  //   expect( commitIndex ).is.equals( 2 );
-  //   expect( destroyIndex ).is.equals( 2 );
-  // });
-
-  // it( 'Hu.directiveFn: 注册的指令方法在被弃用时会触发对应 destroy 方法 ( 插值内切换: 指令方法切换为非指令方法 )', () => {
-  //   let commitIndex = 0;
-  //   let destroyIndex = 0;
-  //   const fn = Hu.directiveFn( class{
-  //     commit( value ){
-  //       commitIndex++;
-  //     }
-  //     destroy(){
-  //       destroyIndex++;
-  //     }
-  //   });
-
-  //   expect( commitIndex ).is.equals( 0 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div>${ fn( 123 ) }</div>
-  //   `;
-  //   expect( commitIndex ).is.equals( 1 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div>${ 123 }</div>
-  //   `;
-  //   expect( commitIndex ).is.equals( 1 );
-  //   expect( destroyIndex ).is.equals( 1 );
-  // });
-
-  // it( 'Hu.directiveFn: 注册的指令方法在被弃用时会触发对应 destroy 方法 ( 插值内切换: 非指令方法切换为指令方法 )', () => {
-  //   let commitIndex = 0;
-  //   let destroyIndex = 0;
-  //   const fn = Hu.directiveFn( class{
-  //     commit( value ){
-  //       commitIndex++;
-  //     }
-  //     destroy(){
-  //       destroyIndex++;
-  //     }
-  //   });
-
-  //   expect( commitIndex ).is.equals( 0 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div>${ 123 }</div>
-  //   `;
-  //   expect( commitIndex ).is.equals( 0 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div>${ fn( 123 ) }</div>
-  //   `;
-  //   expect( commitIndex ).is.equals( 1 );
-  //   expect( destroyIndex ).is.equals( 0 );
-  // });
-
-  // it( 'Hu.directiveFn: 注册的指令方法在被弃用时会触发对应 destroy 方法 ( 插值内切换: 切换为其他指令方法 )', () => {
-  //   let commitIndex1 = 0;
-  //   let destroyIndex1 = 0;
-  //   const fn1 = Hu.directiveFn( class{
-  //     commit( value ){
-  //       commitIndex1++;
-  //     }
-  //     destroy(){
-  //       destroyIndex1++;
-  //     }
-  //   });
-
-  //   let commitIndex2 = 0;
-  //   let destroyIndex2 = 0;
-  //   const fn2 = Hu.directiveFn( class{
-  //     commit( value ){
-  //       commitIndex2++;
-  //     }
-  //     destroy(){
-  //       destroyIndex2++;
-  //     }
-  //   });
-
-  //   expect( commitIndex1 ).is.equals( 0 );
-  //   expect( destroyIndex1 ).is.equals( 0 );
-  //   expect( commitIndex2 ).is.equals( 0 );
-  //   expect( destroyIndex2 ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div>${ fn1( 123 ) }</div>
-  //   `;
-  //   expect( commitIndex1 ).is.equals( 1 );
-  //   expect( destroyIndex1 ).is.equals( 0 );
-  //   expect( commitIndex2 ).is.equals( 0 );
-  //   expect( destroyIndex2 ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div>${ fn2( 123 ) }</div>
-  //   `;
-  //   expect( commitIndex1 ).is.equals( 1 );
-  //   expect( destroyIndex1 ).is.equals( 1 );
-  //   expect( commitIndex2 ).is.equals( 1 );
-  //   expect( destroyIndex2 ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div>${ 123 }</div>
-  //   `;
-  //   expect( commitIndex1 ).is.equals( 1 );
-  //   expect( destroyIndex1 ).is.equals( 1 );
-  //   expect( commitIndex2 ).is.equals( 1 );
-  //   expect( destroyIndex2 ).is.equals( 1 );
-  // });
-
-  // it( 'Hu.directiveFn: 注册的指令方法在被弃用时会触发对应 destroy 方法 ( 插值内切换: 切换为同样的指令方法 )', () => {
-  //   let constructorIndex = 0;
-  //   let commitIndex = 0;
-  //   let destroyIndex = 0;
-  //   const fn = Hu.directiveFn( class{
-  //     constructor(){
-  //       constructorIndex++
-  //     }
-  //     commit( value ){
-  //       commitIndex++;
-  //     }
-  //     destroy(){
-  //       destroyIndex++;
-  //     }
-  //   });
-
-  //   expect( constructorIndex ).is.equals( 0 );
-  //   expect( commitIndex ).is.equals( 0 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div>${ fn( 123 ) }</div>
-  //   `;
-  //   expect( constructorIndex ).is.equals( 1 );
-  //   expect( commitIndex ).is.equals( 1 );
-  //   expect( destroyIndex ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div>${ fn( 456 ) }</div>
-  //   `;
-  //   expect( constructorIndex ).is.equals( 1 );
-  //   expect( commitIndex ).is.equals( 2 );
-  //   expect( destroyIndex ).is.equals( 0 );
-  // });
-
-  // it( 'Hu.directiveFn: 注册指令方法时可以注册 proxy 静态方法来代理指令方法的使用步骤', () => {
-  //   let constructorIndex = 0;
-  //   let commitIndex = 0;
-  //   let destroyIndex = 0;
-  //   let staticUsingStep1Index = 0, staticUsingStep2Index = 0;
-  //   const fn = Hu.directiveFn( class{
-  //     constructor(){
-  //       constructorIndex++
-  //     }
-  //     commit( value ){
-  //       commitIndex++;
-  //     }
-  //     destroy(){
-  //       destroyIndex++;
-  //     }
-
-  //     static proxy( using ){
-  //       staticUsingStep1Index++;
-  //       return ( part ) => {
-  //         staticUsingStep2Index++;
-  //         return using( part );
-  //       }
-  //     }
-  //   });
-
-  //   expect( constructorIndex ).is.equals( 0 );
-  //   expect( commitIndex ).is.equals( 0 );
-  //   expect( destroyIndex ).is.equals( 0 );
-  //   expect( staticUsingStep1Index ).is.equals( 0 );
-  //   expect( staticUsingStep2Index ).is.equals( 0 );
-
-  //   const fn1 = fn( 1, 2, 3 );
-
-  //   expect( constructorIndex ).is.equals( 0 );
-  //   expect( commitIndex ).is.equals( 0 );
-  //   expect( destroyIndex ).is.equals( 0 );
-  //   expect( staticUsingStep1Index ).is.equals( 1 );
-  //   expect( staticUsingStep2Index ).is.equals( 0 );
-
-  //   Hu.render( div )`
-  //     <div>${ fn1 }</div>
-  //   `;
-
-  //   expect( constructorIndex ).is.equals( 1 );
-  //   expect( commitIndex ).is.equals( 1 );
-  //   expect( destroyIndex ).is.equals( 0 );
-  //   expect( staticUsingStep1Index ).is.equals( 1 );
-  //   expect( staticUsingStep2Index ).is.equals( 1 );
-  // });
-
-  // it( 'Hu.directiveFn: 注册的指令方法可以在 constructor 中接收到使用该指令方法的指令', () => {
+  it( 'Hu.directiveFn: 注册的指令方法可以定义 proxy 静态方法以拦截指令使用步骤, 方法首个参数为原本指令使用步骤的方法', () => {
+    let usingResult;
+    const fn = Hu.directiveFn( class {
+      commit(){}
+      static proxy( using, args ){
+        usingResult = using;
+      }
+    });
+
+    expect( fn() ).is.equals( usingResult );
+  });
+
+  it( 'Hu.directiveFn: 注册的指令方法可以定义 proxy 静态方法以拦截指令使用步骤, 方法第二参数为使用指令方法时传入的参数数组', () => {
+    let argsResult;
+    const fn = Hu.directiveFn( class {
+      commit(){}
+      static proxy( using, args ){
+        argsResult = args;
+      }
+    });
+
+    fn( 123 );
+    expect( argsResult ).is.deep.equals([ 123 ]);
+
+    fn( 1, 2, 3 );
+    expect( argsResult ).is.deep.equals([ 1, 2, 3 ]);
+  });
+
+  it( 'Hu.directiveFn: 注册的指令方法可以定义 proxy 静态方法以拦截指令使用步骤, 可以返回一个新的指令使用步骤的方法', () => {
+    const newUsing = () => {};
+    const fn = Hu.directiveFn( class {
+      commit(){}
+      static proxy( using, args ){
+        return newUsing;
+      }
+    });
+
+    expect( fn() ).is.equals( newUsing );
+  });
+
+  it( 'Hu.directiveFn: 注册的指令方法可以定义 proxy 静态方法以拦截指令使用步骤, 返回的方法会在指令使用步骤被调用', () => {
+    let result;
+    const newUsing = ( part ) => {
+      result = part;
+    };
+    const fn = Hu.directiveFn( class {
+      commit(){}
+      static proxy( using, args ){
+        return newUsing;
+      }
+    });
+
+    // 在 Node 指令中使用
+    render(
+      fn(),
+      div
+    );
+    expect( result ).is.instanceOf( NodePart );
+
+    // 在 Attr 指令中使用
+    render( div )`
+      <div class=${ fn() }></div>
+    `;
+    expect( result ).is.instanceOf( AttributePart );
+    // 在 Attr 指令中使用
+    render( div )`
+      <div class="a ${ fn() } b"></div>
+    `;
+    expect( result ).is.instanceOf( AttributePart );
+
+    // 在 Boolean 指令中使用
+    render( div )`
+      <div ?disabled=${ fn() }></div>
+    `;
+    expect( result ).is.instanceOf( BasicBooleanDirective );
+
+    // 在 Event 指令中使用
+    render( div )`
+      <div @click=${ fn() }></div>
+    `;
+    expect( result ).is.instanceOf( BasicEventDirective );
+
+    // 在 Prop 指令中使用
+    render( div )`
+      <div .title=${ fn() }></div>
+    `;
+    expect( result ).is.instanceOf( BasicPropertyDirective );
     
-  // });
+    // 在 Class 指令中使用
+    render( div )`
+      <div :class=${ fn() }></div>
+    `;
+    expect( result ).is.instanceOf( ClassDirective );
+    
+    // 在 Html 指令中使用
+    render( div )`
+      <div :html=${ fn() }></div>
+    `;
+    expect( result ).is.instanceOf( HtmlDirective );
+    
+    // // 在 Model 指令中使用
+    // render( div )`
+    //   <div :model=${ fn() }></div>
+    // `;
+    // expect( result ).is.instanceOf( ModelDirective );
+    
+    // 在 Show 指令中使用
+    render( div )`
+      <div :show=${ fn() }></div>
+    `;
+    expect( result ).is.instanceOf( ShowDirective );
+    
+    // 在 Style 指令中使用
+    render( div )`
+      <div :style=${ fn() }></div>
+    `;
+    expect( result ).is.instanceOf( StyleDirective );
+    
+    // 在 Text 指令中使用
+    render( div )`
+      <div :text=${ fn() }></div>
+    `;
+    expect( result ).is.instanceOf( TextDirective );
+  });
 
 });
