84865bb4761301e7b1c72e63e2090dce224c830a
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-12-13 13:56:37 +0800

    完善单元测试

--- test/Test/index.js
+++ test/Test/index.js
@@ -4,4 +4,5 @@ import './src/options/props';
 import './src/options/data';
 import './src/options/computed';
 import './src/options/methods';
-import './src/options/watch'
+import './src/options/watch';
+import './src/options/styles';
