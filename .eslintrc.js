1f03f3c0110e08c7cf71e2a2cb8249382a8f39bf
wei.zhang <37876877+Zhang-Wei-666@users.noreply.github.com>
2020-03-27 11:51:52 +0800

    开始启用 eslint 进行代码格式化

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  root: true,
+  extends: [
+    "./node_modules/@moomfe/hu-cli/.eslintrc.js"
+  ],
+  rules: {
+
+  }
+
+};
--- hu.config.js
+++ hu.config.js
@@ -1,47 +1,49 @@
 require('./scripts/extras/copy-polyfill');
 const pluginFixErrorInFirefox = require('./scripts/plugins/Fix-SyntaxError-error-in-Firefox-51-and-below');
 const pluginCopyToTest = require('./scripts/plugins/copy-to-test');
+
 const HU_RUNNING_COMMAND = process.env.HU_RUNNING_COMMAND;
 const packages = require('./package.json');
-const banner = `${ packages.title } v${ packages.version }\n${ packages.homepage }\n\n(c) 2018-present ${ packages.author }\nReleased under the MIT License.`;
+
+const banner = `${packages.title} v${packages.version}\n${packages.homepage}\n\n(c) 2018-present ${packages.author}\nReleased under the MIT License.`;
 const configs = [];
 const pipe = [
   { output: 'hu.js' }
 ];
 
 
-if( HU_RUNNING_COMMAND === 'build' ){
+if (HU_RUNNING_COMMAND === 'build') {
   // UMD Minify
-  pipe.$each( config => {
+  pipe.$each((config) => {
     pipe.push(
-      Object.$assign( null, config, {
+      Object.$assign(null, config, {
         mode: true,
-        output: config.output.replace( /\.js$/, '.min.js' )
+        output: config.output.replace(/\.js$/, '.min.js')
       })
     );
   });
   // Other
-  pipe.$each( config => {
+  pipe.$each((config) => {
     // CommonJS
     config.mode || pipe.push(
-      Object.$assign( null, config, {
+      Object.$assign(null, config, {
         format: 'cjs',
-        output: config.output.replace( /\.js$/, '.common.js' )
+        output: config.output.replace(/\.js$/, '.common.js')
       })
     );
     // ES Module
     config.mode || pipe.push(
-      Object.$assign( null, config, {
+      Object.$assign(null, config, {
         format: 'esm',
-        output: config.output.replace( /\.js$/, '.esm.js' )
+        output: config.output.replace(/\.js$/, '.esm.js')
       })
     );
     // ES Module Browser + ES Module Browser Minify
     pipe.push(
-      Object.$assign( null, config, {
+      Object.$assign(null, config, {
         format: 'esm.browser',
-        output: config.mode ? config.output.replace( /\.min\.js$/, '.esm.browser.min.js' )
-                            : config.output.replace( /\.js$/, '.esm.browser.js' )
+        output: config.mode ? config.output.replace(/\.min\.js$/, '.esm.browser.min.js')
+          : config.output.replace(/\.js$/, '.esm.browser.js')
       })
     );
   });
@@ -54,10 +56,10 @@ configs.push({
   name: 'Hu',
   banner,
   replace: {
-    'process.env.NODE_ENV': JSON.stringify( HU_RUNNING_COMMAND === 'build' ? 'production' : 'development' ),
-    '__VERSION__': packages.version
+    'process.env.NODE_ENV': JSON.stringify(HU_RUNNING_COMMAND === 'build' ? 'production' : 'development'),
+    __VERSION__: packages.version
   },
-  plugins: ( config ) => [
+  plugins: (config) => [
     pluginFixErrorInFirefox,
     config._originConfig.output === 'hu.js' && pluginCopyToTest
   ],
@@ -65,7 +67,7 @@ configs.push({
   pipe
 });
 
-if( HU_RUNNING_COMMAND === 'watch' ){
+if (HU_RUNNING_COMMAND === 'watch') {
   configs.push(
     require('./test/Test/hu.config')
   );
