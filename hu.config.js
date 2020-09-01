a5ff9cdaa0a972fca2a10cba0a15ede3ac463c22
wei.zhang <37876877+Zhang-Wei-666@users.noreply.github.com>
2020-09-01 16:14:30 +0800

    初始化项目

--- hu.config.js
+++ hu.config.js
@@ -3,7 +3,22 @@ const HU_RUNNING_COMMAND = process.env.HU_RUNNING_COMMAND;
 
 module.exports = {
   mode: HU_RUNNING_COMMAND === 'build',
+  input: 'index.ts',
   replace: {
     'process.env.NODE_ENV': JSON.stringify(HU_RUNNING_COMMAND === 'build' ? 'production' : 'development')
-  }
+  },
+  pluginOptions: {
+    babel: {
+      babelrc: false,
+      exclude: [/\/node_modules\//],
+      extensions: ['.js', '.ts'],
+      presets: [
+        ['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3, targets: 'browserslist' }],
+        ['@babel/preset-typescript', { allowNamespaces: true }]
+      ]
+    }
+  },
+  browserslist: [
+    'last 2 Chrome versions'
+  ]
 };
---
+++ src/observable/observe.ts
@@ -0,0 +1,9 @@
+/**
+ * 存放原始对象和观察者对象及其选项参数的映射
+ */
+export const observeMap = new WeakMap();
+
+/**
+ * 存放观察者对象和观察者对象选项参数的映射
+ */
+export const observeProxyMap = new WeakMap();
