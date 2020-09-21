735a8834463b445570b458c17c6d4f783402ad01
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2020-09-21 16:37:25 +0800

    完善类型及注释

--- src/observable/observe.ts
+++ src/observable/observe.ts
@@ -6,13 +6,20 @@ import isPlainObject from "../shared/isPlainObject";
 type ObserveTargetType = any[] | {};
 
 /**
+ * 观察者对象类型
+ */
+type ObserveProxyType<T extends ObserveTargetType> = {
+  [K in keyof T]: T[K]
+};
+
+/**
  * 观察者对象选项参数
  */
-interface ObserveOptions {
+interface ObserveOptions<T extends ObserveTargetType> {
   /** 观察者对象的原始对象 */
-  target: ObserveTargetType;
+  target: T;
   /** 观察者对象 */
-  proxy: ObserveTargetType;
+  proxy: ObserveProxyType<T>;
   /** 订阅了当前观察者子集对象更新的 watcher 集合 */
   subs: {};
   /** 订阅了当前观察者对象深度监听的 watcher 集合 */
@@ -26,31 +33,54 @@ interface ObserveOptions {
 /**
  * 存放原始对象和观察者对象及其选项参数的映射
  */
-export const observeMap: WeakMap<ObserveTargetType, ObserveOptions> = new WeakMap();
+export const observeMap: WeakMap<any, any> = new WeakMap();
 
 /**
  * 存放观察者对象和观察者对象选项参数的映射
  */
-export const observeProxyMap: WeakMap<ObserveTargetType, ObserveOptions> = new WeakMap();
+export const observeProxyMap: WeakMap<any, any> = new WeakMap();
 
 
 /**
- * 传入对象, 返回该对象的观察者对象
+ * 传入任意对象, 把支持的对象转为观察者对象后返回, 不支持的对象会被直接返回
  * @param obj 需要创建观察者对象的原始对象
  */
-export function observable<T = any>(obj: T): T {
-  return isPlainObject(obj) || Array.isArray(obj) ? observe(obj) : obj;
+export function observable<T>(obj: T): T extends ObserveTargetType ? ObserveProxyType<T> : T {
+  // @ts-ignore
+  return (isPlainObject(obj) || Array.isArray(obj)) ? observe(obj) : obj;
+}
+
+interface observeOptions {
+  [propName: string]: {
+    before: (target: ObserveTargetType, name: string, targetProxy: ObserveTargetType) => boolean | undefined
+  }
 }
 
 /**
- * ( Private ) 传入对象, 返回该对象的观察者对象
+ * 传入对象, 返回该对象的观察者对象, 如果传入对象本身就是观察者对象, 将直接返回
  * @param target 需要创建观察者对象的原始对象
  * @param options 观察者对象选项参数
  */
-export function observe<T extends ObserveTargetType>(target: T, options?: {}): T {
+export function observe<T>(target: T, options?: observeOptions): ObserveProxyType<T>{
+  // 如果创建过观察者
+  // 则返回之前创建的观察者
+  if (observeMap.has(target)) return observeMap.get(target).proxy;
+  // 如果传入的就是观察者对象
+  // 则直接返回
+  if (observeProxyMap.has(target)) return target;
+  // 否则立即创建观察者进行返回
+  return createObserver(target, options);
+}
+
+/**
+ * 传入对象, 返回该对象的观察者对象
+ * @param target 需要创建观察者对象的原始对象
+ * @param options 观察者对象选项参数
+ */
+function createObserver<T extends ObserveTargetType>(target: T, options?: observeOptions): ObserveProxyType<T> {
   // 创建观察者对象选项参数
   // @ts-ignore
-  const observeOptions: ObserveOptions = {
+  const observeOptions: ObserveOptions<T> = {
     target,
     subs: Object.create(null),
     deepSubs: new Set(),
@@ -59,7 +89,7 @@ export function observe<T extends ObserveTargetType>(target: T, options?: {}): T
   };
 
   // 创建观察者对象
-  const proxy = observeOptions.proxy = new Proxy(target, {
+  const proxy: ObserveProxyType<T> = observeOptions.proxy = new Proxy(target, {
 
   });
 
@@ -68,4 +98,12 @@ export function observe<T extends ObserveTargetType>(target: T, options?: {}): T
   observeProxyMap.set(proxy, observeOptions);
 
   return proxy;
+}
+
+
+/**
+ * 创建依赖收集的响应方法
+ */
+function createObserverProxyGetter() {
+
 }
